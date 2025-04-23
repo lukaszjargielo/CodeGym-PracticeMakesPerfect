192309411b24623f52f70695a2345773dc44b5f8
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:16:15 +0200

    Task 8 (Level 1/Lesson 6/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0118/Solution.java
@@ -0,0 +1,20 @@
+package en.codegym.task.jdk13.task01.task0118;
+
+/* 
+Comment out the superfluous
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int a = 10;
+        int b = 6;
+        int b = 12;
+        int sum = 1 + a + b;
+        int sum = 2 + a + b;
+        int sum = 3 + a + b;
+        int sum = 4 + a + b;
+
+        System.out.println("sum = " + sum);
+    }
+}
