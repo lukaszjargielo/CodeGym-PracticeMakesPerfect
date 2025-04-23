c4d1fe8039517b9995b20a7e0bc407d5715bddc4
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:55:34 +0200

    Task 11 (Level 2/Lesson 3/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0110/Solution.java
@@ -0,0 +1,11 @@
+package en.codegym.task.jdk13.task01.task0110;
+
+/* 
+A variable on our screen
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
