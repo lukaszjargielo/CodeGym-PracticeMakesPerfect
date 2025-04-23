ff0380f026a4dc5d1e7161fdb56d2607f7b2902e
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:28:43 +0200

    Task 9 (Level 1/Lesson 8/Task 1) - My solution

--- src/en/codegym/task/jdk13/task01/task0106/Solution.java
+++ src/en/codegym/task/jdk13/task01/task0106/Solution.java
@@ -6,6 +6,6 @@ Bugs and features
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        System.out.println("It's not a bug - it's a feature.");
     }
 }
