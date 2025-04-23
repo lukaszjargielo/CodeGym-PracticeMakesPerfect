ac8875f98c59f92b9f3c333ff3f6fab4d97bafe3
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 13:42:53 +0200

    Task 5 (Level 1/Lesson 6/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0107/Solution.java
@@ -0,0 +1,21 @@
+package en.codegym.task.jdk13.task01.task0107;
+
+/* 
+No comments needed
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        // y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
