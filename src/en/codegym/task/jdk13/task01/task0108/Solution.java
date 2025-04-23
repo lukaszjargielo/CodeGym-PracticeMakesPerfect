c2f7d8ef10f48cc809a183af81c074b17e92e6ef
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:07:10 +0200

    Task 6 (Level 1/Lesson 6/Task 2) - My solution

--- src/en/codegym/task/jdk13/task01/task0108/Solution.java
+++ src/en/codegym/task/jdk13/task01/task0108/Solution.java
@@ -1,4 +1,4 @@
-gpackage en.codegym.task.jdk13.task01.task0108;
+package en.codegym.task.jdk13.task01.task0108;
 
 /* 
 2 + 3 = 5
@@ -9,15 +9,15 @@ public class Solution {
         int a = 3;
         int b = 2;
 
-        System.out.print("two");
+        //System.out.print("two");
         System.out.print(b);
         System.out.print(" plus ");
-        System.out.print(" minus ");
+        //System.out.print(" minus ");
         System.out.print(a);
-        System.out.print("three");
+        //System.out.print("three");
         System.out.print(" is equal to ");
-        System.out.print(" equals ");
-        System.out.print("five");
+        //System.out.print(" equals ");
+        //System.out.print("five");
         System.out.print(a + b);
     }
 }
