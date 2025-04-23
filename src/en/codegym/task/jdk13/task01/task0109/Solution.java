75518a1c52816bdefb987498ac8e55e0cd478bea
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:47:38 +0200

    Task 10 (Level 2/Lesson 3/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0109/Solution.java
@@ -0,0 +1,13 @@
+
+package en.codegym.task.jdk13.task01.task0109;
+
+/* 
+We solemnly publish and declare these variables...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+    }
+}
