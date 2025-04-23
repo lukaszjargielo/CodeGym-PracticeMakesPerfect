0697ffb91a35e52b39b7349e8fa532e8804f42a8
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 13:04:15 +0200

    Task 3 (Level 1/Lesson 4/Task 2) - My solution

--- src/en/codegym/task/jdk13/task01/task0103/Solution.java
+++ src/en/codegym/task/jdk13/task01/task0103/Solution.java
@@ -5,7 +5,8 @@ My young friend
 */
 
 public class Solution {
+    private static int currentYear = 3126;
     public static void main(String[] args) {
-        //write your code here
+        System.out.println(currentYear - 8);
     }
 }
