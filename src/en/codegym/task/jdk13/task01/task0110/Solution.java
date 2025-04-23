5a371a486a7d05609c946b17b39d9082128c558e
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:57:34 +0200

    Task 11 (Level 2/Lesson 3/Task 2) - My solution

--- src/en/codegym/task/jdk13/task01/task0110/Solution.java
+++ src/en/codegym/task/jdk13/task01/task0110/Solution.java
@@ -6,6 +6,7 @@ A variable on our screen
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+       String name = "Java";
+        System.out.println(name);
     }
 }
