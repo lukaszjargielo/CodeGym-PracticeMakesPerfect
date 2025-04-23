e758785e4a0ed1f166683f3e3367e1912b1607f9
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 11:33:48 +0200

    Task 1 (Level 1/Lesson 3/Task 1) - My solution

--- src/en/codegym/task/jdk13/task01/task0101/Solution.java
+++ src/en/codegym/task/jdk13/task01/task0101/Solution.java
@@ -6,6 +6,6 @@ It's cool to be a programmer!
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        System.out.println("I think being a programmer is cool");
     }
 }
