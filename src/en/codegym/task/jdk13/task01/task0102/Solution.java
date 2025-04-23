fce55e84c4885d8b1a47215ff37e7dc0f9cd8683
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 12:09:47 +0200

    Task 2 (Level 1/Lesson 4/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0102/Solution.java
@@ -0,0 +1,11 @@
+package en.codegym.task.jdk13.task01.task0102;
+
+/* 
+Kind words for the teacher
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
