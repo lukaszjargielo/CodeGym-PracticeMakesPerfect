e2bf3de45cd82390fd601ea4c1b071f88e81f1c1
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 11:10:46 +0200

    Added .gitignore file and content of first task

---
+++ src/en/codegym/task/jdk13/task01/task0101/Solution.java
@@ -0,0 +1,11 @@
+ package en.codegym.task.jdk13.task01.task0101;
+
+/* 
+It's cool to be a programmer!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
