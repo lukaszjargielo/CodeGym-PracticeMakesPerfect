000e2c54b27f19fd5eb690835e59a31a65a81bf9
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 18:47:20 +0200

    Task 18 (Level 2/Lesson 6/Task 1) - My solution

--- src/com/codegym/task/task01/task0121/Solution.java
+++ src/com/codegym/task/task01/task0121/Solution.java
@@ -7,6 +7,12 @@ Contract
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        System.out.println("My name is Amigo.\n");
+        System.out.printf("I agree to wages of %s/month in the %s year.\n","$5000", "first");
+        System.out.printf("I agree to wages of %s/month in the %s year.\n","$5500", "second");
+        System.out.printf("I agree to wages of %s/month in the %s year.\n","$7000", "third");
+        System.out.printf("I agree to wages of %s/month in the %s year.\n","$8000", "fourth");
+        System.out.printf("I agree to wages of %s/month in the %s year.\n","$10000", "fifth");
+        System.out.println("\nKiss my shiny metal rear actuator!");
     }
 }
