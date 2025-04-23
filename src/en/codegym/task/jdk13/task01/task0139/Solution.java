f7fbf7c0acc8fad738d24e3f420100f5bc9b4265
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 18:29:38 +0200

    Task 17 (Level 2/Lesson 5/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0139/Solution.java
@@ -0,0 +1,11 @@
+package en.codegym.task.jdk13.task01.task0139;
+
+/* 
+Kiss my shiny metal rear actuator
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
