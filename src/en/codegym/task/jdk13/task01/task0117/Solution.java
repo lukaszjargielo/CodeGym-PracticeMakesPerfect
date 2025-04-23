ef1031086288690f2642405d578514bf3460087b
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 18:22:41 +0200

    Task 15 (Level 2/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0117/Solution.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task01.task0117;
+
+/* 
+A few more corrections
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string1 = "Coding";
+        String string2 = "in";
+        String string3 = "Pascal";
+        System.out.println(string1 + " " + string2 + " " + string3);
+    }
+}
