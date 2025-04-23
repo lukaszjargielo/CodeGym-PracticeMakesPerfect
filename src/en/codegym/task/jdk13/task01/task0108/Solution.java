4666e95b0ccecd2eebd21c666859937e033c2933
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:03:22 +0200

    Task 6 (Level 1/Lesson 6/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0108/Solution.java
@@ -0,0 +1,23 @@
+gpackage en.codegym.task.jdk13.task01.task0108;
+
+/* 
+2 + 3 = 5
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        System.out.print("two");
+        System.out.print(b);
+        System.out.print(" plus ");
+        System.out.print(" minus ");
+        System.out.print(a);
+        System.out.print("three");
+        System.out.print(" is equal to ");
+        System.out.print(" equals ");
+        System.out.print("five");
+        System.out.print(a + b);
+    }
+}
