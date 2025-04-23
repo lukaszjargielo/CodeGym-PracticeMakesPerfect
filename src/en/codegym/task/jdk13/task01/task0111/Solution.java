acd4af85cf98196c8dff65bb0739df88cb720dfe
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-23 17:59:42 +0200

    Task 12 (Level 2/Lesson 3/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task01/task0111/Solution.java
@@ -0,0 +1,11 @@
+package en.codegym.task.jdk13.task01.task0111;
+
+/* 
+One variable is not enough
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
