0aefac00e72889c54dfc5efffb17cc18d5eb2512
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 10:01:21 +0200

    Task 21 (Level 2/Lesson 7/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task03/task0322/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task03.task0322;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Deep and pure love
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
