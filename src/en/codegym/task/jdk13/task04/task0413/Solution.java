1e2ad379da86a0f02bcef175745fd019cadc480f
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 13:24:55 +0200

    Task 32 (Level 3/Lesson 7/Task 4) - My solution

--- src/en/codegym/task/jdk13/task04/task0413/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0413/Solution.java
@@ -9,7 +9,32 @@ Day of the week
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int intDayOfTheWeek = Integer.parseInt(reader.readLine());
+        String returnedDay = "";
 
+        returnedDay = getDayName(intDayOfTheWeek);
+        System.out.println(returnedDay);
+    }
+
+    public static String getDayName(int intDayOfTheWeek) {
+        switch (intDayOfTheWeek) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return "No such day of the week";
+        }
     }
 }
