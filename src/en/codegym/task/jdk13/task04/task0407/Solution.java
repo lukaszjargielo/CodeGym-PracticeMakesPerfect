68a9f5aed5ddfb1cca8111ff533426484af6052f
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 11:47:45 +0200

    Task 28 (Level 3/Lesson 5/Task 5) - My solution

--- src/en/codegym/task/jdk13/task04/task0407/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0407/Solution.java
@@ -7,10 +7,10 @@ Count the number of cats
 public class Solution {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
-        //write your code here
+        Cat.count++;
 
         Cat cat2 = new Cat();
-        //write your code here
+        Cat.count++;
 
         System.out.println("The cat count is " + Cat.count);
     }
