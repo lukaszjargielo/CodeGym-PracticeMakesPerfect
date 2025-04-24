08bd6b3d542ae4ebae9cc8f57c288d8632412d8c
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 11:00:22 +0200

    Task 25 (Level 3/Lesson 5/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0404/Cat.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task04.task0404;
+
+/* 
+Cat register
+*/
+
+public class Cat {
+    private static int catCount = 0;
+
+    public static void addNewCat() {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
