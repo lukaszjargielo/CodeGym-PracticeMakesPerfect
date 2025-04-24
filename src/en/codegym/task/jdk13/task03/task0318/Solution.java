a56c370cf151c4163371e25697adf2e03dc2c53f
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 09:47:17 +0200

    Task 19 (Level 2/Lesson 7/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task03/task0318/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task03.task0318;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+How to take over the world
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
