562d31a115bbb74863cb48334045916a55624acb
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 12:42:39 +0200

    Task 30 (Level 3/Lesson 7/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0410/Solution.java
@@ -0,0 +1,20 @@
+package en.codegym.task.jdk13.task04.task0410;
+
+/* 
+Interval
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        checkInterval(60);
+        checkInterval(112);
+        checkInterval(10);
+    }
+
+    public static void checkInterval(int a) {
+        //write your code here
+        System.out.println("The number " + a + " is in the interval.");
+        System.out.println("The number " + a + " is not in the interval.");
+
+    }
+}
