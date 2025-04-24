7a737a641f09f08f3baef63bda8fd13710b5fa1e
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 16:25:08 +0200

    Task 35 (Level 3/Lesson 8/Task 3) - My solution

--- src/en/codegym/task/jdk13/task04/task0423/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0423/Solution.java
@@ -9,7 +9,13 @@ Bouncer policy
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
 
+        if(age > 20) {
+            System.out.println("18 is old enough");
+        }
 
     }
 }
