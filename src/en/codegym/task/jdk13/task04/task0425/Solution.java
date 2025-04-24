7235d9e7927e9c7df361f43b748feebbd7f3e6a1
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 16:39:50 +0200

    Task 36 (Level 3/Lesson 8/Task 4) - My solution

--- src/en/codegym/task/jdk13/task04/task0425/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0425/Solution.java
@@ -9,7 +9,36 @@ Quadrants
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(reader.readLine());
+        int y = Integer.parseInt(reader.readLine());
+
+        if(x > 0) {
+            if(y > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
+        } else {
+            if (y > 0) {
+                System.out.println(2);
+            } else {
+                System.out.println(3);
+            }
+        }
+
+   /*     Probably better solution - second way
+            if(x > 0 && y > 0) {
+                System.out.println(1);
+            } else if (x > 0 && y < 0) {
+                System.out.println(4);
+            } else if (x < 0 && y < 0) {
+                System.out.println(3);
+            } else if (x < 0 && y > 0) {
+                System.out.println(2);
+            }
+            All conditions are visible
+            */
 
     }
 }
