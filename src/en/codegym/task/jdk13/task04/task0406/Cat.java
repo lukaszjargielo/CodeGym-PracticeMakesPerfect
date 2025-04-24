5d61836825f92d5f938ade6a60a726efbaf2b6c8
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 11:08:23 +0200

    Task 27 (Level 3/Lesson 5/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0406/Cat.java
@@ -0,0 +1,19 @@
+package en.codegym.task.jdk13.task04.task0406;
+
+/* 
+Name register
+*/
+
+public class Cat {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
