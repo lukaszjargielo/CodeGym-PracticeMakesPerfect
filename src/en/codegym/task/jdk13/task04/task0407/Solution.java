74e9baf38ad8a70dca56156bbafdd58772522727
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 11:45:29 +0200

    Task 28 (Level 3/Lesson 5/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0407/Solution.java
@@ -0,0 +1,21 @@
+package en.codegym.task.jdk13.task04.task0407;
+
+/* 
+Count the number of cats
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        //write your code here
+
+        Cat cat2 = new Cat();
+        //write your code here
+
+        System.out.println("The cat count is " + Cat.count);
+    }
+
+    public static class Cat {
+        public static int count = 0;
+    }
+}
