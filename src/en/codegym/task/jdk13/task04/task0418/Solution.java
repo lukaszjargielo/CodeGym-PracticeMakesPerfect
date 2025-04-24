20e5be22d31d5210417e40df0579bb6229a12d46
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 14:15:46 +0200

    Task 33 (Level 3/Lesson 8/Task 1) - My solution

--- src/en/codegym/task/jdk13/task04/task0418/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0418/Solution.java
@@ -9,7 +9,12 @@ Minimum of two numbers
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+
+        int minNumber = num1 < num2 ? num1 : num2;
+        System.out.println(minNumber);
 
     }
 }
