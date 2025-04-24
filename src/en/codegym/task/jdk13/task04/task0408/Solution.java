d83670dbefd174058cdb3f8f25f274b80bf1b02e
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 12:32:09 +0200

    Task 29 (Level 3/Lesson 7/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0408/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task04.task0408;
+
+/* 
+Good or bad?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        compare(3);
+        compare(6);
+        compare(5);
+    }
+
+    public static void compare(int a) {
+        //write your code here
+
+    }
+}
