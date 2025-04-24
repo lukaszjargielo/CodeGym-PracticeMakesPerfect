9726d4c4c11d8b20f398d4b2f7c736ea5d3a259d
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 14:11:06 +0200

    Task 33 (Level 3/Lesson 8/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0418/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task04.task0418;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Minimum of two numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
