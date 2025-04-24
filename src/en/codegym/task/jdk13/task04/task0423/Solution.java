e02295a075f3579e784f7c98f51f74742e2b48e5
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-24 16:21:17 +0200

    Task 35 (Level 3/Lesson 8/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0423/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task04.task0423;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Bouncer policy
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
