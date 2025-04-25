e8002f4549cf0fd09ab250f975a42752e0ced8a0
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 11:43:15 +0200

    Task 40 (Level 3/Lesson 10/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0429/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task04.task0429;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Positive and negative numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
