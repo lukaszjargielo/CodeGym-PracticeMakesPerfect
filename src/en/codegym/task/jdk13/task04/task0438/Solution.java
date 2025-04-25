775e1d1305ea6688057c2908cfc949e951b8f338
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 18:42:15 +0200

    Task 48 (Level 4/Lesson 6/Task 4) - My solution

--- src/en/codegym/task/jdk13/task04/task0438/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0438/Solution.java
@@ -6,7 +6,13 @@ Drawing lines
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        for(int i = 0; i < 10; i++) {
+            System.out.print(8);
+        }
+        System.out.println();
 
+        for(int i = 0; i < 10; i ++) {
+            System.out.println(8);
+        }
     }
 }
