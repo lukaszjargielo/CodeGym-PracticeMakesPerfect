283f6fa91dca19cb9c21209c579566200f7cafde
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 11:13:18 +0200

    Task 39 (Level 3/Lesson 10/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0428/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task04.task0428;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Positive number
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+}
