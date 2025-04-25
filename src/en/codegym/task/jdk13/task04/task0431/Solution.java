b065125d24842578750d0e33c58df318d2ac8c92
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 16:39:28 +0200

    Task 42 (Level 4/Lesson 4/Task 2) - My solution

--- src/en/codegym/task/jdk13/task04/task0431/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0431/Solution.java
@@ -6,7 +6,9 @@ From 10 to 1
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-
+        int number = 10;
+        while(number > 0) {
+            System.out.println(--number);
+        }
     }
 }
