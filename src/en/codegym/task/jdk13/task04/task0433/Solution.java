c2b9d056a4f3f958e61f0bc4e540c255c2a7f3d0
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 16:48:51 +0200

    Task 44 (Level 4/Lesson 4/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0433/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task04.task0433;
+
+/* 
+Seeing dollars in your future
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
