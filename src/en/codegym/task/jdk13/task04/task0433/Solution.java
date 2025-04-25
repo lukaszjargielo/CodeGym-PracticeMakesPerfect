dd391ae213e489315793dd1b1a14c86f55739880
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 16:57:54 +0200

    Task 44 (Level 4/Lesson 4/Task 4) - My solution

--- src/en/codegym/task/jdk13/task04/task0433/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0433/Solution.java
@@ -6,7 +6,26 @@ Seeing dollars in your future
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-
+        int innerCounter = 0;
+        int outerCounter = 0;
+        while (outerCounter < 10) {
+            while (innerCounter < 10) {
+                System.out.print("$");
+                innerCounter++;
+            }
+            System.out.println();
+            innerCounter = 0;
+            outerCounter++;
+        }
     }
 }
+
+/*
+Recommended solution
+      int i = 0;
+
+        while (i < 10) {
+            System.out.println("$$$$$$$$$$");
+            i++;
+        }
+ */
