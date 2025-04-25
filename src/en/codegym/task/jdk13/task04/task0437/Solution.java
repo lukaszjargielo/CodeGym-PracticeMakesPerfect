c58bc1404aca15f40ec5935162b4868992386efa
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 18:37:23 +0200

    Task 47 (Level 4/Lesson 6/Task 3) - My solution

--- src/en/codegym/task/jdk13/task04/task0437/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0437/Solution.java
@@ -6,7 +6,11 @@ Triangle of eights
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
