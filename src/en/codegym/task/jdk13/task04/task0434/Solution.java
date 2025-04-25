c271bf073f00b12579ee0dfb03aeb94064aaa023
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 17:34:21 +0200

    Task 45 (Level 4/Lesson 5/Task 1) - My solution

--- src/en/codegym/task/jdk13/task04/task0434/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0434/Solution.java
@@ -6,7 +6,11 @@ Multiplication table
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
+                System.out.print(j*i + " ");
+            }
+            System.out.println();
+        }
     }
 }
