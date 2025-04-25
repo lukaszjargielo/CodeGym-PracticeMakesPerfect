d794304fb42b15280f335006912943e714e2499a
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 16:41:12 +0200

    Task 43 (Level 4/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0432/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task04.task0432;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+You can't have too much of a good thing
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
