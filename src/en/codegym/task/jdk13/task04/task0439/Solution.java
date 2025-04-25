4d9c2f3d1fd47906338beac0206e75433f9eb140
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 18:43:47 +0200

    Task 49 (Level 4/Lesson 6/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0439/Solution.java
@@ -0,0 +1,16 @@
+
+package en.codegym.task.jdk13.task04.task0439;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Chain letter
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
