ae772812b6bce3cad70b962e92dd3f18ec6c9cea
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 17:49:32 +0200

    Task 46 (Level 4/Lesson 6/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0436/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task04.task0436;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Drawing a rectangle
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
