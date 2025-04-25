07e82fa559988cdbe65221f271b3f4ebfe6a3b60
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 18:33:19 +0200

    Task 47 (Level 4/Lesson 6/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0437/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task04.task0437;
+
+/* 
+Triangle of eights
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
