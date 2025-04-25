0aee8d60613065d1e8d891ed27570298923de2c5
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 11:05:11 +0200

    Task 38 (Level 3/Lesson 10/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0427/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task04.task0427;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Describing numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+
+    }
+}
