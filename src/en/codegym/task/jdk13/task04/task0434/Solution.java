5f13477a2796c5ff2f71354e92dd32a7afc9bfdb
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 17:05:22 +0200

    Task 45 (Level 4/Lesson 5/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0434/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task04.task0434;
+
+/* 
+Multiplication table
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
