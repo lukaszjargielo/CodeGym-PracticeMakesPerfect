122a71ba1199bfda6b33fb2510c4ab23409ce5c0
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 10:08:14 +0200

    Task 37 (Level 3/Lesson 10/Task 1) - My solution

--- src/en/codegym/task/jdk13/task04/task0426/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0426/Solution.java
@@ -9,7 +9,19 @@ Labels and numbers
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if ((number % 2 == 0) && (number > 0)) {
+            System.out.println("Positive even number");
+        } else if ((number % 2 == 0) && (number < 0)) {
+            System.out.println("Negative even number");
+        } else if ((number % 2 != 0) && (number > 0)) {
+            System.out.println("Positive odd number");
+        } else if ((number % 2 != 0) && (number < 0)) {
+            System.out.println("Negative odd number");
+        } else if (number == 0) {
+            System.out.println("Zero");
+        }
     }
 }
+
