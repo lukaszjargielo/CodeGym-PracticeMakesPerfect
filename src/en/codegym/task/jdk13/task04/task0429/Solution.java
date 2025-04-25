e39fd4aaa172be3af94e77907a413afd0685f6bb
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 12:34:34 +0200

    Task 40 (Level 3/Lesson 10/Task 4) - My solution

--- src/en/codegym/task/jdk13/task04/task0429/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0429/Solution.java
@@ -9,7 +9,57 @@ Positive and negative numbers
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+        int num3 = Integer.parseInt(reader.readLine());
 
+        int[] numbers = {num1, num2, num3};
+
+        checkNumberSign(numbers);
+    }
+
+    private static void checkNumberSign(int[] numbers) {
+        int positiveNumbersCounter = 0;
+        int negativeNumbersCounter = 0;
+        for(int number : numbers) {
+            if (number > 0) {
+                positiveNumbersCounter++;
+            } else if (number < 0) {
+                negativeNumbersCounter++;
+            }
+        }
+
+        System.out.println("Number of negative numbers: " + negativeNumbersCounter);
+        System.out.println("Number of positive numbers: " + positiveNumbersCounter);
     }
 }
+
+/*
+Recommended solution
+
+   BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number1 = Integer.parseInt(reader.readLine());
+        int number2 = Integer.parseInt(reader.readLine());
+        int number3 = Integer.parseInt(reader.readLine());
+        int countPositive = 0;
+        int countNegative = 0;
+        if (number1 > 0) {
+            countPositive++;
+        } else if (number1 < 0) {
+            countNegative++;
+        }
+        if (number2 > 0) {
+            countPositive++;
+        } else if (number2 < 0) {
+            countNegative++;
+        }
+        if (number3 > 0) {
+            countPositive++;
+        } else if (number3 < 0) {
+            countNegative++;
+        }
+        System.out.println("Number of negative numbers: " + countNegative);
+        System.out.println("Number of positive numbers: " + countPositive);
+    }
+ */
