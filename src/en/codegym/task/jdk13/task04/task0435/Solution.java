5dde1a5de6c7b933dc1759effeb1650021ed2ac1
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 17:42:21 +0200

    Task 46 (Level 4/Lesson 6/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0435/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task04.task0435;
+
+/* 
+Even numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
