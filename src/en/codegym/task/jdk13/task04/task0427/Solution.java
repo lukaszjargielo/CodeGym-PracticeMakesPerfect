f78cac2106ce89f417effe832f13a8907f49bef8
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 11:08:05 +0200

    Task 38 (Level 3/Lesson 10/Task 2) - My solution

--- src/en/codegym/task/jdk13/task04/task0427/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0427/Solution.java
@@ -9,8 +9,69 @@ Describing numbers
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number > 0 && number < 1000) {
 
+            String parity = checkParity(number);
+            String numberOfDigits = checkNumberOfDigits(number);
 
+            System.out.println(parity + " " + numberOfDigits);
+        }
+    }
+
+    private static String checkParity(int number) {
+        if (number % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+
+    private static String checkNumberOfDigits(int number) {
+        if (number < 10) {
+            return "single-digit number";
+        } else if (number < 100) {
+            return "two-digit number";
+        } else {
+            return "three-digit number";
+        }
     }
 }
+
+/*First way
+      if (number % 2 == 0) {
+                if (number < 10) {
+                    System.out.println("even single-digit number");
+                } else if (number < 100) {
+                    System.out.println("even two-digit number");
+                } else {
+                    System.out.println("even three-digit number");
+                }
+
+            } else {
+                if (number < 10) {
+                    System.out.println("odd single-digit number");
+                } else if (number < 100) {
+                    System.out.println("odd two-digit number");
+                } else {
+                    System.out.println("odd three-digit number");
+                }
+            }
+
+Recommended way
+
+if (number > 0 && number < 10 && number % 2 == 0) {
+            System.out.println("even single-digit number");
+        } else if (number > 0 && number < 10 && number % 2 != 0) {
+            System.out.println("odd single-digit number");
+        } else if (number > 9 && number < 100 && number % 2 == 0) {
+            System.out.println("even two-digit number");
+        } else if (number > 9 && number < 100 && number % 2 != 0) {
+            System.out.println("odd two-digit number");
+        } else if (number > 99 && number < 1000 && number % 2 == 0) {
+            System.out.println("even three-digit number");
+        } else if (number > 99 && number < 1000 && number % 2 != 0) {
+            System.out.println("odd three-digit number");
+        }
+ */
