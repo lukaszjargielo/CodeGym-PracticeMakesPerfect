08276f928bcc1d3bbe213f4e6e337be4dc229571
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 18:39:11 +0200

    Task 48 (Level 4/Lesson 6/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0438/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task04.task0438;
+
+/* 
+Drawing lines
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
