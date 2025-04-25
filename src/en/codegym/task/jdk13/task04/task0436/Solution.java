0e83be23a3b283d365d50706200c8049d9a54443
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-25 18:31:17 +0200

    Task 46 (Level 4/Lesson 6/Task 2) - My solution

--- src/en/codegym/task/jdk13/task04/task0436/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0436/Solution.java
@@ -9,7 +9,16 @@ Drawing a rectangle
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
 
     }
 }
