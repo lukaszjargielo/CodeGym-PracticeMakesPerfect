97a3958bcd2d68d8dbf48b3aa5d5735a54af77cd
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-26 14:04:38 +0200

    Task 50 (Level 4/Lesson 10/Task 1) - My solution

--- src/en/codegym/task/jdk13/task04/task0440/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0440/Solution.java
@@ -6,7 +6,8 @@ Decent pay
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I will never work for peanuts. Amigo");
+        }
     }
 }
