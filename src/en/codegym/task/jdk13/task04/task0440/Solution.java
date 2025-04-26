2da7acbd4a1c7a8b40dbbab13dc7f7d485031ca3
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-26 14:01:25 +0200

    Task 50 (Level 4/Lesson 10/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task04/task0440/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task04.task0440;
+
+/* 
+Decent pay
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
