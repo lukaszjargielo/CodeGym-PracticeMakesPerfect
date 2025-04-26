96b2999810c2f6e483144b9f038978b901217c34
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-26 14:27:29 +0200

    Task 51 (Level 4/Lesson 10/Task 2) - My solution

--- src/en/codegym/task/jdk13/task04/task0441/Solution.java
+++ src/en/codegym/task/jdk13/task04/task0441/Solution.java
@@ -2,6 +2,7 @@ package en.codegym.task.jdk13.task04.task0441;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Somewhere in the middle
@@ -9,7 +10,25 @@ Somewhere in the middle
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+        int num3 = Integer.parseInt(reader.readLine());
 
+        int[] numbers = {num1, num2, num3};
+        Arrays.sort(numbers);
+        System.out.println(numbers[1]);
     }
 }
+
+/*
+Recommended solution
+
+    if ((number1 >= number2 && number1 <= number3) || (number1 <= number2 && number1 >= number3)) {
+            System.out.println(number1);
+        } else if ((number2 >= number1 && number2 <= number3) || (number2 <= number1 && number2 >= number3)) {
+            System.out.println(number2);
+        } else {
+            System.out.println(number3);
+        }
+ */
