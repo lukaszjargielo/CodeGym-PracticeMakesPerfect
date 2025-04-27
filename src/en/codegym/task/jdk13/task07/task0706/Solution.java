c1e61ffc700fde26bda030738502ad50ccef977f
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-27 16:36:29 +0200

    Task 59 (Level 6/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0706/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task07.task0706;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Street and houses
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //write your code here
+    }
+}
