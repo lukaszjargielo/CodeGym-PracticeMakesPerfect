730c975f952062ba2cb25a7ba5e47a38fd6e7691
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-27 14:17:08 +0200

    Task 56 (Level 6/Lesson 3/Task 2) - My solution

--- src/en/codegym/task/jdk13/task07/task0704/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0704/Solution.java
@@ -9,7 +9,16 @@ Array of numbers in reverse order
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-       // write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length ; i++) {
+            numbers[i] = Integer.parseInt(reader.readLine());
+        }
+
+        for (int i = numbers.length-1; i >=0 ; i--) {
+            System.out.println(numbers[i]);
+        }
     }
 }
 
