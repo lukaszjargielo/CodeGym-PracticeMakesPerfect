c3b5f6e5e920acd44e557074889db35b2e078955
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-27 16:14:05 +0200

    Task 58 (Level 6/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0705/Solution.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task07.task0705;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+One large array and two small ones
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+}
