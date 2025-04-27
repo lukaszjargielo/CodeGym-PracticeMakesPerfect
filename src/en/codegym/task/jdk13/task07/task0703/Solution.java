907e5ab130c7917bd4f3a92d7c0cc5cb5ca4a8d4
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-27 15:48:43 +0200

    Task 57 (Level 6/Lesson 4/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0703/Solution.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task07.task0703;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+2 arrays
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+}
