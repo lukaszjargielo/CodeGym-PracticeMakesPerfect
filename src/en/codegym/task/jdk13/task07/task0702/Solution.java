a3a45ac8d169804070343df3175ed936adeb857e
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-27 13:57:27 +0200

    Task 55 (Level 6/Lesson 3/Task 1) - My solution

--- src/en/codegym/task/jdk13/task07/task0702/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0702/Solution.java
@@ -9,6 +9,15 @@ String array in reverse order
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[10];
+
+        for (int i = 0; i < 8; i++) {
+            array[i] = reader.readLine();
+        }
+
+        for (int i = array.length-1; i >= 0 ; i--) {
+            System.out.println(array[i]);
+        }
     }
 }
