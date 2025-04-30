c87b1b53bb9d30452b48127b7567a7e7108fce9f
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 09:34:34 +0200

    Task 61 (Level 6/Lesson 5/Task 2) - My solution

--- src/en/codegym/task/jdk13/task06/task0634/Solution.java
+++ src/en/codegym/task/jdk13/task06/task0634/Solution.java
@@ -1,5 +1,7 @@
 package en.codegym.task.jdk13.task06.task0634;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /* 
@@ -9,7 +11,47 @@ Chess board
 public class Solution {
     public static char[][] array;
 
-    public static void main(String[] args) {
-        //write your code here
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int chessboardDimension = Integer.parseInt(reader.readLine());
+
+        array = new char[chessboardDimension][chessboardDimension];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if(i % 2 == 0 && j % 2 == 0) {
+                    array[i][j] = '#';
+                }else if(i % 2 != 0 && j % 2 != 0) {
+                    array[i][j] = '#';
+                } else {
+                    array[i][j] = ' ';
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
+
+/*
+Recommended solution
+
+  Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        array = new char[number][number];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if ((i + j) % 2 == 0) {
+                    array[i][j] = '#';
+                } else {
+                    array[i][j] = ' ';
+                }
+            }
+        }
+ */
