febe28a0454fb17236974bacf9ec956b5d98c630
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 11:04:45 +0200

    Task 64 (Level 6/Lesson 6/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task05/task0511/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.pro.task05.task0511;
+
+import java.util.Scanner;
+
+/* 
+Creating a two-dimensional array
+*/
+
+public class Solution {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
