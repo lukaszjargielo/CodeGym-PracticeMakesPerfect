3e761a51834b0ad7e3064eb6d5756ea23cf6a01d
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 09:39:30 +0200

    Task 62 (Level 6/Lesson 5/Task 3) - Requirements

---
+++ src/en/codegym/task/pro/task05/task0509/Solution.java
@@ -0,0 +1,14 @@
+package en.codegym.task.pro.task05.task0509;
+
+/* 
+Multiplication table
+*/
+
+public class Solution {
+
+    public static int[][] MULTIPLICATION_TABLE;
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
