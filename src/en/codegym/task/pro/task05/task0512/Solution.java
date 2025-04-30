3b69e8b6d9a8d04ab2349622ecee1edaf9b7153e
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 12:22:27 +0200

    Task 65 (Level 6/Lesson 6/Task 3) - My solution

--- src/en/codegym/task/pro/task05/task0512/Solution.java
+++ src/en/codegym/task/pro/task05/task0512/Solution.java
@@ -4,11 +4,27 @@ package en.codegym.task.pro.task05.task0512;
 Creating a multi-array
 */
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
 
     public static void main(String[] args) {
-        //write your code here
+        String string = Arrays.deepToString(multiArray);
+        System.out.println(string);
     }
 }
+/*
+Recommended solution
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.print(multiArray[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("----------");
+        }
+    }
+ */
