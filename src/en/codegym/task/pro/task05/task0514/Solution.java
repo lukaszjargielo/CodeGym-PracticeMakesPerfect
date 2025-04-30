46037abfefd6bcd89233c1e1dc544cfe58e3fbb9
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 13:56:40 +0200

    Task 67 (Level 6/Lesson 7/Task 2) - My solution

--- src/en/codegym/task/pro/task05/task0514/Solution.java
+++ src/en/codegym/task/pro/task05/task0514/Solution.java
@@ -13,6 +13,7 @@ public class Solution {
     public static int[][] ints = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 
     public static void main(String[] args) {
-        //write your code here
+        System.out.println(Arrays.deepToString(strings));
+        System.out.println(Arrays.deepToString(ints));
     }
 }
