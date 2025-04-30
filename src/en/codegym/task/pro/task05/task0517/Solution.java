abc965b82464a8fb6af4c007f99ba16776b161bd
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 17:15:15 +0200

    Task 70 (Level 6/Lesson 8/Task 2) - My solution

--- src/en/codegym/task/pro/task05/task0517/Solution.java
+++ src/en/codegym/task/pro/task05/task0517/Solution.java
@@ -12,7 +12,20 @@ public class Solution {
     public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
     public static void main(String[] args) {
-        //write your code here
+        int firstHalf = (array.length % 2) == 0 ? array.length / 2 : array.length / 2 + 1;
+        int[] ints1 = Arrays.copyOfRange(array, 0, firstHalf);
+        result[0] = new int [ints1.length];
+        result[0] =  ints1;
+        int[] ints2 = Arrays.copyOfRange(array, firstHalf, array.length);
+        result[1] = new int [ints2.length];
+        result[1] = ints2;
         System.out.println(Arrays.deepToString(result));
     }
 }
+/*
+Recommended solution
+int firstPart = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
+        result[0] = Arrays.copyOfRange(array, 0, firstPart);
+        result[1] = Arrays.copyOfRange(array, firstPart, array.length);
+        System.out.println(Arrays.deepToString(result));
+ */
