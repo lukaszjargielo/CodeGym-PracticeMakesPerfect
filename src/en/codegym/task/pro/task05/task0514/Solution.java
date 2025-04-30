4ad88e8eda701e86119d68f244d1f7215d176153
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 13:53:43 +0200

    Task 67 (Level 6/Lesson 7/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task05/task0514/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.pro.task05.task0514;
+
+import java.util.Arrays;
+
+/* 
+Displaying two-dimensional arrays
+*/
+
+public class Solution {
+
+    public static String[][] strings = new String[][]{{"🙂", "🙂", "🙂", "🙂"},
+            {"🙃", "🙃", "🙃", "🙃"}, {"😋", "😋", "😋", "😋"}, {"😎", "😎", "😎", "😎"}};
+    public static int[][] ints = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
