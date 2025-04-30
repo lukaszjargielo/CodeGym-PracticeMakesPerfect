79dd00abbe68b22c58a475bd3be4200b8762d2e5
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 17:26:32 +0200

    Task 71 (Level 6/Lesson 8/Task 3) - My solution

--- src/en/codegym/task/pro/task05/task0518/Solution.java
+++ src/en/codegym/task/pro/task05/task0518/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
     public static void main(String[] args) {
-        //write your code here
+        Arrays.sort(array);
         System.out.println(Arrays.toString(array));
     }
 }
