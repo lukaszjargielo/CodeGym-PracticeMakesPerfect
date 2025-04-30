173b2c1e468f6a93b1626235c695c3cd259ac689
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 16:57:57 +0200

    Task 70 (Level 6/Lesson 8/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task05/task0517/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.pro.task05.task0517;
+
+import java.util.Arrays;
+
+/* 
+Splitting an array
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[2][];
+    public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println(Arrays.deepToString(result));
+    }
+}
