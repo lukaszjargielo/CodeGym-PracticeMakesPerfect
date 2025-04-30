01a9d3ef78edd46a18d8d926e12c8c569afbdcd4
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 16:18:41 +0200

    Task 69 (Level 6/Lesson 8/Task 1) - Requirements

---
+++ src/en/codegym/task/pro/task05/task0516/Solution.java
@@ -0,0 +1,19 @@
+package en.codegym.task.pro.task05.task0516;
+
+import java.util.Arrays;
+
+/* 
+Filling an array
+*/
+
+public class Solution {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println(Arrays.toString(array));
+    }
+}
