401179c156b80378703238a001576a9e9abdeae5
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 09:11:47 +0200

    Task 61 (Level 6/Lesson 5/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0634/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task06.task0634;
+
+import java.util.Scanner;
+
+/* 
+Chess board
+*/
+
+public class Solution {
+    public static char[][] array;
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
