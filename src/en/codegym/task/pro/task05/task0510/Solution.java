86112692029896f10c9ac4516995d66736340b2b
lukaszjargielo <l.jargielo88@gmail.com>
2025-04-30 10:46:56 +0200

    Task 63 (Level 6/Lesson 6/Task 1) - Requirements

---
+++ src/en/codegym/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,14 @@
+package en.codegym.task.pro.task05.task0510;
+
+/* 
+Triangular array
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
