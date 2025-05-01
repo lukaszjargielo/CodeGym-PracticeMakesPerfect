2957768ef7e8f318aff8b9b462c5d3e44dd449db
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-01 14:17:15 +0200

    Task 74 (Level 7/Lesson 1/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task06/task0602/Solution.java
@@ -0,0 +1,21 @@
+package en.codegym.task.pro.task06.task0602;
+
+/* 
+Orderly information
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        print1();
+        print2();
+    }
+
+    public static void print1() {
+        System.out.println("For sweetest things turn sourest by their deeds;");
+    }
+
+    public static void print2() {
+        System.out.println("Lilies that fester smell far worse than weeds.");
+    }
+}
