ffab33e69f85b843356beab44532fd6f0a36e65b
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-01 17:28:20 +0200

    Task 77 (Level 7/Lesson 2/Task 3) - Requirements

---
+++ src/en/codegym/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,26 @@
+package en.codegym.task.pro.task06.task0605;
+
+/* 
+Correct order
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        //write your code here
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
