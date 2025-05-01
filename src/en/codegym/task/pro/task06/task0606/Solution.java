6b9a51407c95de5f17331e05c2e362c7af27fdff
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-01 18:08:06 +0200

    Task 78 (Level 7/Lesson 2/Task 4) - My solution

--- src/en/codegym/task/pro/task06/task0606/Solution.java
+++ src/en/codegym/task/pro/task06/task0606/Solution.java
@@ -14,5 +14,39 @@ public class Solution {
 
     }
 
-    //write your code here
+    public static void universalMethod(int number) {
+
+    }
+
+    public static void universalMethod(String text) {
+
+    }
+
+    public static void universalMethod(double number) {
+
+    }
+
+    public static void universalMethod(int number1, double number2) {
+
+    }
+
+    public static void universalMethod(double number1, int number2) {
+
+    }
+
+    public static void universalMethod(int number1, double number2, String text) {
+
+    }
+
+    public static void universalMethod(int number1, String text, double number2) {
+
+    }
+
+    public static void universalMethod(String text, int number1, double number2) {
+
+    }
+
+    public static void universalMethod(String text, double number1, int number2) {
+
+    }
 }
