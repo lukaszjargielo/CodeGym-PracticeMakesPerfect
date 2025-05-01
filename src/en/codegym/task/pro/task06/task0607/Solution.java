2573665ecd106ae0e8273d009d0172a116d2b9dd
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-01 18:27:25 +0200

    Task 79 (Level 7/Lesson 3/Task 1) - Requirements

---
+++ src/en/codegym/task/pro/task06/task0607/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.pro.task06.task0607;
+
+/* 
+Welcome! But not everyone.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void signIn(String username) {
+        //write your code here
+        System.out.println("Welcome, " + username);
+        System.out.println("We've really missed you, " + username);
+    }
+}
