cdae1972e2d04881f2336e79b31c6d2b18c5859c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-01 18:41:53 +0200

    Task 80 (Level 7/Lesson 3/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,13 @@
+package en.codegym.task.pro.task06.task0608;
+
+/* 
+A cube calculator
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+}
