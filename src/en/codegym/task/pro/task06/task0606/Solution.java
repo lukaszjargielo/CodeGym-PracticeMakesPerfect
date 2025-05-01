ec10dfe02a3b8a0b7ca71201409aab7ab4cd648c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-01 17:56:11 +0200

    Task 78 (Level 7/Lesson 2/Task 4) - Requirements

---
+++ src/en/codegym/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.pro.task06.task0606;
+
+/* 
+A method for all seasons
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    //write your code here
+}
