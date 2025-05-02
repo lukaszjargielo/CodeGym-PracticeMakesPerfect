69217382626b7a7d11a93df5cfcb0a0049eab23a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-02 11:32:24 +0200

    Task 85 (Level 7/Lesson 4/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0218/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task02.task0218;
+
+/* 
+Repetition is the mother of all learning
+*/
+
+public class Solution {
+    public static void print3(String text) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
