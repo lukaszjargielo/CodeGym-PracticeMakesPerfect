9e2b23efd384b969dd142c1abc37c855292c4164
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-02 12:07:46 +0200

    Task 87 (Level 7/Lesson 4/Task 6) - My solution

--- src/com/codegym/task/task01/task0136/Solution.java
+++ src/com/codegym/task/task01/task0136/Solution.java
@@ -6,11 +6,15 @@ Even to the moon!
 */
 
 public class Solution {
+    public static final double LUNAR_GRAVITY_COEFFICIENT = 0.17;
+
     public static void main(String[] args) {
         System.out.println(getWeight(888));
     }
 
     public static double getWeight(int earthWeight) {
-        // write your code here
+        return earthWeight * LUNAR_GRAVITY_COEFFICIENT;
     }
+
+
 }
