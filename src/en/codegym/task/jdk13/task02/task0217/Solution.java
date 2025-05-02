4c9b134db97bbeada9b2036ca62ed84dc6b1e2db
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-02 11:25:30 +0200

    Task 84 (Level 7/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0217/Solution.java
@@ -0,0 +1,25 @@
+package en.codegym.task.jdk13.task02.task0217;
+
+/* 
+Minimum of four numbers
+*/
+
+public class Solution {
+    public static int min(int a, int b, int c, int d) {
+        //write your code here
+
+    }
+
+    public static int min(int a, int b) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+}
