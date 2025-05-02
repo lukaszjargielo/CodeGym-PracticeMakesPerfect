1e9593a86e1c53ae796e04acbb2053b5efaf44e0
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-02 11:12:36 +0200

    Task 81 (Level 7/Lesson 3/Task 3) - My solution

--- src/en/codegym/task/pro/task06/task0609/Solution.java
+++ src/en/codegym/task/pro/task06/task0609/Solution.java
@@ -8,10 +8,11 @@ public class Solution {
     public static void main(String[] args) {
 
     }
-
     public static long cube(long a) {
         return a * a * a;
     }
 
-    //write your code here
+    public static long ninthDegree(long number) {
+        return cube(cube(number));
+    }
 }
