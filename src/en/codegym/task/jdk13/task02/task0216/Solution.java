4bfcb0c760b83eee7aaa347265fa2535fd43abab
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-02 11:19:34 +0200

    Task 83 (Level 7/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0216/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task02.task0216;
+
+/* 
+Minimum of three numbers
+*/
+
+public class Solution {
+    public static int min(int a, int b, int c) {
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
