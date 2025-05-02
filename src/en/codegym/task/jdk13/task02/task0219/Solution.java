5b32938f5894ecd690b1411be3b107aecad5485e
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-02 11:50:03 +0200

    Task 86 (Level 7/Lesson 4/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0219/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task02.task0219;
+
+/* 
+Print three times
+*/
+
+public class Solution {
+    public static void print3(String text) {
+      // write your code here
+
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+}
