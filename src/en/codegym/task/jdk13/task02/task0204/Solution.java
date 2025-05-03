4ccbe3a3aa757a18c56550f27d767ee24abfacd9
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 16:42:48 +0200

    Task 99 (Level 8/Lesson 3/Task 2) - My solution

--- src/en/codegym/task/jdk13/task02/task0204/Solution.java
+++ src/en/codegym/task/jdk13/task02/task0204/Solution.java
@@ -6,7 +6,10 @@ Family relations
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Man man = new Man();
+        Woman woman = new Woman();
+        man.wife = woman;
+        woman.husband = man;
     }
 
     public static class Man {
