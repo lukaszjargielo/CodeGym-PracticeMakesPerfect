5b734d9c2249bce4b72698db05d1d7c8453e5308
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 10:40:14 +0200

    Task 93 (Level 7/Lesson 7/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task06/task0615/Earth.java
@@ -0,0 +1,12 @@
+package en.codegym.task.pro.task06.task0615;
+
+/* 
+Everything you need to know about Earth
+*/
+
+public class Earth {
+    String name = "Earth";
+    double square = 510_100_000;
+    long population = 7_594_000_000L;
+    long equatorLength = 40_075_696;
+}
