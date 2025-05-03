cfb7691e659ffb8c06a847b2f4061f485708b8f8
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 16:50:46 +0200

    Task 100 (Level 8/Lesson 3/Task 3) - My solution

--- src/en/codegym/task/jdk13/task02/task0209/Solution.java
+++ src/en/codegym/task/jdk13/task02/task0209/Solution.java
@@ -9,7 +9,11 @@ public class Solution {
         Dog dog1 = new Dog();
         dog1.name = "Max";
 
-        //write your code here
+        Dog dog2 = new Dog();
+        dog2.name = "Bella";
+
+        Dog dog3 = new Dog();
+        dog3.name = "Jack";
     }
 
     public static class Dog {
