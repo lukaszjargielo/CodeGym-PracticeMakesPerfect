232a3425e464945c99cc9b7e4be7d0dfcec4472c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 13:36:22 +0200

    Task 97 (Level 8/Lesson 2/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0831/Solution.java
@@ -0,0 +1,23 @@
+package en.codegym.task.jdk13.task08.task0831;
+
+import java.util.Arrays;
+
+/* 
+Favorite board games
+*/
+
+public class Solution {
+
+    public static BoardGame[] collection = new BoardGame[5];
+
+    public static void main(String[] args) {
+        BoardGame chess = new BoardGame();
+        chess.name = "Battleship";
+        collection[0] = chess;
+
+        //write your code here
+
+
+        System.out.println(Arrays.toString(collection));
+    }
+}
