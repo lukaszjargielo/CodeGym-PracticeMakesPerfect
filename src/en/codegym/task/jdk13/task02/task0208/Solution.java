10fd16d08218ad52b7f5a3b25822f18704e91108
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 16:04:21 +0200

    Task 98 (Level 8/Lesson 3/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0208/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task02.task0208;
+
+/* 
+Cats are good
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+
+    public static class Cat {
+
+    }
+}
