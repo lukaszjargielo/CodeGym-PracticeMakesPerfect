58222d030282c625ee49267b924998cb4e41e198
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 13:30:02 +0200

    Task 96 (Level 8/Lesson 2/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0202/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task02.task0202;
+
+/* 
+Where does a Person come from?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+
+    public static class Person {
+        //write your code here
+    }
+}
