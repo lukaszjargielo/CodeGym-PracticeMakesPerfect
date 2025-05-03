f7b60456398e5a61805bc8830e1a42f18181fb08
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 10:38:44 +0200

    Task 92 (Level 7/Lesson 7/Task 1) - My solution

--- src/en/codegym/task/pro/task06/task0614/Solution.java
+++ src/en/codegym/task/pro/task06/task0614/Solution.java
@@ -5,7 +5,7 @@ Remembering geometry
 */
 
 public class Solution {
-    public static double pi = 3.14;
+    public static final double pi = 3.14;
     public static double radius = 2.5;
     public static double diameter = 5;
 
