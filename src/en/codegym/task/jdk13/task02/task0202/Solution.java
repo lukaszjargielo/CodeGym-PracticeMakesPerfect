b96420c222cd4b72359e5f4e2f8bbc5b84d11adc
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-03 16:46:14 +0200

    Re-adding a solved task to staging area

---
+++ src/en/codegym/task/jdk13/task02/task0202/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task02.task0202;
+
+/* 
+Where does a Person come from?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+
+    public static class Person {
+        private String name;
+        private int age;
+        private int weight;
+        private int money;
+    }
+}
