958a5931b142fe739127ccf8732cbd28479a2230
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-04 13:19:28 +0200

    Task 103 (Level 8/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0213/Solution.java
@@ -0,0 +1,26 @@
+package en.codegym.task.jdk13.task02.task0213;
+
+/* 
+Every animal should have an owner
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
