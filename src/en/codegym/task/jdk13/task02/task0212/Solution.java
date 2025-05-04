559aa2b2108961c2bc3ea8c510448b879b58e642
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-04 13:04:15 +0200

    Task 102 (Level 8/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task02/task0212/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task02.task0212;
+
+/* 
+Crazy eights
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        Cat cat8 = new Cat();
+        Cat cat9;
+        //write your code here
+    }
+
+    public static class Cat {
+
+    }
+}
