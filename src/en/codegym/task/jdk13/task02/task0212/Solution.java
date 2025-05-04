6a9a2539ff0746a2f0b815bb69a61014c7d5030e
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-04 13:18:12 +0200

    Task 102 (Level 8/Lesson 4/Task 2) - My solution

--- src/en/codegym/task/jdk13/task02/task0212/Solution.java
+++ src/en/codegym/task/jdk13/task02/task0212/Solution.java
@@ -4,12 +4,21 @@ package en.codegym.task.jdk13.task02.task0212;
 Crazy eights
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Cat cat = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
         Cat cat8 = new Cat();
         Cat cat9;
-        //write your code here
+        Cat cat10;
     }
 
     public static class Cat {
