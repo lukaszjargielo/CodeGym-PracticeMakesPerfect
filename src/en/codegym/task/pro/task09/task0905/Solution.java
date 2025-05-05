553a56a4e39d924e386ae985a6b3798bf78ca6c2
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-05 12:57:09 +0200

    Task 108 (Level 9/Lesson 2/Task 1) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0905/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.pro.task09.task0905;
+
+/* 
+Octal converter
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Decimal number " + decimalNumber + " is equal to octal number " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Octal number " + octalNumber + " is equal to decimal number " + toDecimal(octalNumber));
+    }
+
+    public static int toOctal(int decimalNumber) {
+        //write your code here
+        return 0;
+    }
+
+    public static int toDecimal(int octalNumber) {
+        //write your code here
+        return 0;
+    }
+}
