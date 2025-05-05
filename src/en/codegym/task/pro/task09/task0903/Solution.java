66980df602a72305ae5447738a23ab74bc25402a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-05 12:00:24 +0200

    Task 106 (Level 9/Lesson 1/Task 3) - My solution

--- src/en/codegym/task/pro/task09/task0903/Solution.java
+++ src/en/codegym/task/pro/task09/task0903/Solution.java
@@ -5,5 +5,9 @@ String literals
 */
 
 public class Solution {
-    public String text = "Always write code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live. Martin Golding";
+    public String text = "Always write code as if " +
+            "the guy who ends up maintaining your code will be " +
+            "a violent psychopath " +
+            "who knows where you live. " +
+            "Martin Golding";
 }
