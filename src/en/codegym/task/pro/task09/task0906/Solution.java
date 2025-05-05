d77ba393cf87944fbfc79c3fb4fa299110a9c17f
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-05 14:19:24 +0200

    Task 109 (Level 9/Lesson 2/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0906/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.pro.task09.task0906;
+
+/* 
+Binary converter
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = Integer.MAX_VALUE;
+        System.out.println("Decimal number " + decimalNumber + " is equal to binary number " + toBinary(decimalNumber));
+        String binaryNumber = "1111111111111111111111111111111";
+        System.out.println("Binary number " + binaryNumber + " is equal to decimal number " + toDecimal(binaryNumber));
+    }
+
+    public static String toBinary(int decimalNumber) {
+        //write your code here
+        return null;
+    }
+
+    public static int toDecimal(String binaryNumber) {
+        //write your code here
+        return 0;
+    }
+}
