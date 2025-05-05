466567d8d3955b9c3538d6fbbad3907360c0872a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-05 11:00:33 +0200

    Task 104 (Level 9/Lesson 1/Task 1) - My solution

--- src/en/codegym/task/pro/task09/task0901/Solution.java
+++ src/en/codegym/task/pro/task09/task0901/Solution.java
@@ -5,8 +5,8 @@ Integer literals
 */
 
 public class Solution {
-    public byte b = 128;
-    public short s = 32768;
-    public int i = 1_234_567_890L;
-    public long l = 2_345_678_900;
+    public byte b = -128;
+    public short s = -32768;
+    public int i = 1_234_567_890;
+    public long l = 2_345_678_900L;
 }
