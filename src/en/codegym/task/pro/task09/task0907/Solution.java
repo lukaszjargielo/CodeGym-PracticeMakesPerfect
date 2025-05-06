3fc3a50fd668a87a91ddfa6c803758ff05ba0021
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-06 09:56:45 +0200

    Task 110 (Level 9/Lesson 2/Task 3) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0907/Solution.java
@@ -0,0 +1,26 @@
+package en.codegym.task.pro.task09.task0907;
+
+/* 
+Hexadecimal converter
+*/
+
+public class Solution {
+    private static final String HEX = "0123456789abcdef";
+
+    public static void main(String[] args) {
+        int decimalNumber = 1256;
+        System.out.println("Decimal number " + decimalNumber + " is equal to hexadecimal number " + toHex(decimalNumber));
+        String hexNumber = "4e8";
+        System.out.println("Hexadecimal number " + hexNumber + " is equal to decimal number " + toDecimal(hexNumber));
+    }
+
+    public static String toHex(int decimalNumber) {
+        //write your code here
+        return null;
+    }
+
+    public static int toDecimal(String hexNumber) {
+        //write your code here
+        return 0;
+    }
+}
