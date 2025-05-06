5cf21cc3ff65a0eed99e51efed92c6a8d6b97cd3
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-06 12:32:17 +0200

    Task 111 (Level 9/Lesson 2/Task 4) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0908/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.pro.task09.task0908;
+
+/* 
+Binary to hexadecimal converter
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String binaryNumber = "100111010000";
+        System.out.println("Binary number " + binaryNumber + " is equal to hexadecimal number " + toHex(binaryNumber));
+        String hexNumber = "9d0";
+        System.out.println("Hexadecimal number " + hexNumber + " is equal to binary number " + toBinary(hexNumber));
+    }
+
+    public static String toHex(String binaryNumber) {
+        //write your code here
+        return null;
+    }
+
+    public static String toBinary(String hexNumber) {
+        //write your code here
+        return null;
+    }
+}
