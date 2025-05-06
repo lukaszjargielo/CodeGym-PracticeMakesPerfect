6e0bf31acf40827a33332498098b4c8e5b84b41a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-06 09:46:53 +0200

    Task 109 (Level 9/Lesson 2/Task 2) - My solution

--- src/en/codegym/task/pro/task09/task0906/Solution.java
+++ src/en/codegym/task/pro/task09/task0906/Solution.java
@@ -13,12 +13,57 @@ public class Solution {
     }
 
     public static String toBinary(int decimalNumber) {
-        //write your code here
-        return null;
+        String binaryNumber = "";
+        if(decimalNumber > 0) {
+            while (decimalNumber != 0) {
+                binaryNumber = decimalNumber % 2 + binaryNumber;
+                decimalNumber = decimalNumber / 2;
+            }
+        }
+        return binaryNumber;
     }
 
     public static int toDecimal(String binaryNumber) {
-        //write your code here
-        return 0;
+        int decimalNumber = 0;
+        if(binaryNumber != null && !binaryNumber.isEmpty()) {
+            for (int i = 0; i < binaryNumber.length(); i++) {
+                int digit = binaryNumber.charAt(binaryNumber.length() - 1 - i) - '0';
+                if(digit != 0 && digit != 1) {
+                    throw new IllegalArgumentException("Invalid binary digit " + digit);
+                }
+                decimalNumber += digit * (int) Math.pow(2,i);
+            }
+        }
+        return decimalNumber;
     }
 }
+/*
+Recommended solution
+public static String toBinary(int decimalNumber) {
+        String binaryNumber = "";
+        if (decimalNumber <= 0) {
+            return binaryNumber;
+        }
+
+        while (decimalNumber != 0) {
+            binaryNumber = decimalNumber % 2 + binaryNumber;
+            decimalNumber = decimalNumber / 2;
+        }
+        return binaryNumber;
+    }
+
+    public static int toDecimal(String binaryNumber) {
+        int decimalNumber = 0;
+        if (binaryNumber == null) {
+            return decimalNumber;
+        }
+
+        for (int i = 0; i < binaryNumber.length(); i++) {
+            int index = binaryNumber.length() - 1 - i;
+            int value = Character.getNumericValue(binaryNumber.charAt(index));
+            decimalNumber += value * Math.pow(2, i);
+        }
+        return decimalNumber;
+    }
+}
+ */
