c4c659103f6fcc3c72885ee37806f3a0ea8ac4c3
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-07 16:19:41 +0200

    Task 117 (Level 9/Lesson 5/Task 3) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,19 @@
+package en.codegym.task.pro.task09.task0914;
+
+/* 
+Path update
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        //write your code here
+        return null;
+    }
+}
