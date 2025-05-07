235c3b7979c00d5d9d0503f0aba33505247cd020
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-07 09:59:42 +0200

    Task 113 (Level 9/Lesson 3/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0910/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.pro.task09.task0910;
+
+/* 
+Unicode encoding
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] symbols = new char[9];
+        init(symbols);
+        System.out.println(new String(symbols));
+    }
+
+    public static void init(char[] symbols) {
+        //write your code here
+    }
+}
