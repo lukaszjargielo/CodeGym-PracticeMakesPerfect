5d67d9b992a70200a1bcd61fc8eff09e43cbd1a1
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-07 09:33:58 +0200

    Task 112 (Level 9/Lesson 3/Task 1) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0909/Solution.java
@@ -0,0 +1,11 @@
+package en.codegym.task.pro.task09.task0909;
+
+/* 
+Escaping characters
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
