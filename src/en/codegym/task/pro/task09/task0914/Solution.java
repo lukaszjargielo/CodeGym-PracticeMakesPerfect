bbeab47cd888e626e8c5b4d5046ef88bef82336c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-08 11:40:56 +0200

    Task 117 (Level 9/Lesson 5/Task 3) - My solution

--- src/en/codegym/task/pro/task09/task0914/Solution.java
+++ src/en/codegym/task/pro/task09/task0914/Solution.java
@@ -13,7 +13,41 @@ public class Solution {
     }
 
     public static String changePath(String path, String jdk) {
-        //write your code here
-        return null;
+        String newPath = "At least one parameter is invalid";
+        if ((path != null && !path.isEmpty()) && (jdk != null && !jdk.isEmpty())) {
+            int indexStart = path.indexOf("jdk");
+            int indexStop = path.indexOf("/", indexStart);
+            String oldJdk = path.substring(indexStart, indexStop);
+            newPath = path.replace(oldJdk, jdk);
+            return newPath;
+        }
+        return newPath;
     }
 }
+/*
+My second solution
+    public static String changePath(String path, String jdk) {
+        if ((path != null && !path.isEmpty()) && (jdk != null && !jdk.isEmpty())) {
+            String newPath = path.replaceFirst("jdk.+?/",   jdk + "/");
+            return newPath;
+        }
+        return "At least one parameter is invalid";
+    }
+
+My third solution
+    public static String changePath(String path, String jdk) {
+        String newPath = "At least one parameter is invalid";
+        if ((path != null && !path.isEmpty()) && (jdk != null && !jdk.isEmpty())) {
+            String[] split = path.split("\\/");
+            for (int i = 0; i < split.length; i++) {
+                if (split[i].startsWith("jdk")) {
+                    split[i] = jdk;
+                    break;
+                }
+            }
+            newPath = String.join("/", split);
+            ;
+        }
+        return newPath;
+    }
+ */
