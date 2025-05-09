635735b072792dc1bda9efaf78985c954e45958f
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-09 16:30:29 +0200

    Task 119 (Level 9/Lesson 6/Task 2) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0916/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.pro.task09.task0916;
+
+/* 
+String.format()
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(format("Amigo", 5000));
+    }
+
+    public static String format(String name, int salary) {
+        String phrase = "My name is . I will earn $ a month.";
+        //write your code here
+        return null;
+    }
+}
