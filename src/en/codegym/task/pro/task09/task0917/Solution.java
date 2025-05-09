33ae381a31ccb6ad972c4747d884ae20c070a726
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-09 16:46:38 +0200

    Task 120 (Level 9/Lesson 6/Task 3) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0917/Solution.java
@@ -0,0 +1,20 @@
+ package en.codegym.task.pro.task09.task0917;
+
+/* 
+String pool
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String first = new String("CodeGym");
+        String second = new String("CodeGym");
+        String third = new String("codegym");
+        System.out.println(equal(first, second));
+        System.out.println(equal(second, third));
+    }
+
+    public static boolean equal(String first, String second) {
+        //write your code here
+        return false;
+    }
+}
