179d4406e0557c01c1eba79cf348f3559188bdb4
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-10 14:02:13 +0200

    Task 121 (Level 9/Lesson 7/Task 1) - Requirements

---
+++ src/en/codegym/task/pro/task09/task0918/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.pro.task09.task0918;
+
+/* 
+Let's work with StringBuilder
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Learn, learn, and learn again! ";
+
+        System.out.println(addTo(string, new String[]{"A", "rolling", "stone", "gathers", "no", "moss"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        //write your code here
+        return null;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        //write your code here
+        return null;
+    }
+}
