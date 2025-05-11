92ad871d97c2846907ee969086b56fda9c095ba4
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-11 16:06:24 +0200

    Task 129 (Level 10/Lesson 2/Task 2) - My solution

--- src/en/codegym/task/jdk13/task05/task0501/Solution.java
+++ src/en/codegym/task/jdk13/task05/task0501/Solution.java
@@ -6,13 +6,22 @@ Cat carnage (1)
 
 public class Solution {
     public static void main(String[] args) {
+        Cat cat1 = new Cat("Litchi", 4, 8, 6);
+        Cat cat2 = new Cat("Mango", 4, 9, 10);
+        Cat cat3 = new Cat("Melon", 2, 6, 5);
 
     }
-
     public static class Cat {
        private String name;
        private int age;
        private int weight;
        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
     }
 }
