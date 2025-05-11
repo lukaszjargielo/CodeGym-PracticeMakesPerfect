dbc61f4c72ed62e91085ee8d2894fe5d1025f8e8
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-11 15:53:51 +0200

    Task 128 (Level 10/Lesson 2/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0501/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task05.task0501;
+
+/* 
+Cat carnage (1)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Cat {
+        //write your code here
+    }
+}
