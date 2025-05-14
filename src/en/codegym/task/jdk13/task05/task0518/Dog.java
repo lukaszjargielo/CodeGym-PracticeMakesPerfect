e96a479234e6d11a75e64bd6ec7f81ad5f7d094a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 18:43:52 +0200

    Task 147 (Level 10/Lesson 9/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0518/Dog.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task05.task0518;
+
+/* 
+Dog registration
+*/
+
+
+public class Dog {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
