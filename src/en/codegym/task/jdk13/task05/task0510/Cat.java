9fd5bc31c43b4de2bed153826ade1f8fc81a4a7a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:31:57 +0200

    Task 134 (Level 10/Lesson 4/Task 4) - My solution

--- src/en/codegym/task/jdk13/task05/task0510/Cat.java
+++ src/en/codegym/task/jdk13/task05/task0510/Cat.java
@@ -5,7 +5,51 @@ Initializing cats
 */
 
 public class Cat {
-    //write your code here
+    private String name;
+    private int age;
+    private int weight;
+    private String address;
+    private String color;
+
+    public void initialize(String name) {
+        this.name = name;
+        this.age = 5;
+        this.weight = 6;
+        this.address = null;
+        this.color = "tricolor";
+    }
+
+    public void initialize(String name, int weight, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+        this.address = null;
+        this.color = "tricolor";
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = 6;
+        this.address = null;
+        this.color = "tricolor";
+    }
+
+    public void initialize(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+        this.name = null;
+        this.age = 5;
+        this.address = null;
+    }
+
+    public void initialize(int weight, String color, String address) {
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+        this.name = null;
+        this.age = 5;
+    }
 
     public static void main(String[] args) {
 
