8c1ad2972d73343c75e8f1184626b23382d84fbe
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:19:57 +0200

    Task 134 (Level 10/Lesson 4/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0510/Cat.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0510;
+
+/* 
+Initializing cats
+*/
+
+public class Cat {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
