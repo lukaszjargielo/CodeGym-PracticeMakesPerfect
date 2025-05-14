95404010af7e934b4e5b74eabc0a03b81a36011e
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 10:44:54 +0200

    Task 134 (Level 10/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0513/Rectangle.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0513;
+
+/* 
+Let's put together a rectangle
+*/
+
+public class Rectangle {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
