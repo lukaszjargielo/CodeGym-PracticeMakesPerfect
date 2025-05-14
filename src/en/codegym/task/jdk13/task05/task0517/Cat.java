8b7d89b44e8ebcd1c1de2e8c75f54a3fc021f2ad
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 16:53:39 +0200

    Task 142 (Level 10/Lesson 6/Task 5) - My solution

--- src/en/codegym/task/jdk13/task05/task0517/Cat.java
+++ src/en/codegym/task/jdk13/task05/task0517/Cat.java
@@ -5,7 +5,45 @@ Creating cats
 */
 
 public class Cat {
-    //write your code here
+    private String name;
+    private int age;
+    private int weight;
+    private String address;
+    private String color;
+
+    public Cat(String name) {
+        this.name = name;
+        this.age = 5;
+        this.weight = 6;
+        this.color = "tricolor";
+    }
+
+    public Cat(String name, int weight, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+        this.color = "tricolor";
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = 6;
+        this.color = "tricolor";
+    }
+
+    public Cat(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+        this.age = 5;
+    }
+
+    public Cat(int weight, String color, String address) {
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+        this.age = 5;
+    }
 
     public static void main(String[] args) {
 
