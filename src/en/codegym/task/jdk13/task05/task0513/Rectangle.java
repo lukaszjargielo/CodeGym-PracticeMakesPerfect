e6151039b9347a8f95f7c57ef2687ea6b4f749d4
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:00:17 +0200

    Task 134 (Level 10/Lesson 4/Task 3) - My solution

--- src/en/codegym/task/jdk13/task05/task0513/Rectangle.java
+++ src/en/codegym/task/jdk13/task05/task0513/Rectangle.java
@@ -5,7 +5,34 @@ Let's put together a rectangle
 */
 
 public class Rectangle {
-    //write your code here
+    private int top, left, width, height;
+
+    public void initialize(int top, int left, int width, int height){
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+
+    public void initialize(int top, int left){
+        this.top = top;
+        this.left = left;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public void initialize(int width){
+        this.width = width;
+        this.height = width;
+    }
+
+    public void initialize(Rectangle anotherRectangle){
+        this.top = anotherRectangle.top;
+        this.left = anotherRectangle.left;
+        this.width = anotherRectangle.width;
+        this.height = anotherRectangle.height;
+    }
 
     public static void main(String[] args) {
 
