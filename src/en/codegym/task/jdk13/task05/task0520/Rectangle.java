2b1288ce79b0c94b062b4b281afb42a9997d38be
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 16:34:56 +0200

    Task 140 (Level 10/Lesson 6/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0520/Rectangle.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task05.task0520;
+
+/* 
+Populate the Rectangle class
+*/
+
+
+public class Rectangle {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
