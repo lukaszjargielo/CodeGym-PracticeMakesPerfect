13d6f00b9b2cc0012b656db244e358acbdb370e8
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 18:28:20 +0200

    Task 145 (Level 10/Lesson 9/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0525/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.jdk13.task05.task0525;
+
+/* 
+The whole duck isn't enough
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Duck duck1 = new Duck();
+        Duck duck2 = new Duck();
+        System.out.println(duck1);
+        System.out.println(duck2);
+
+        //write your code here
+    }
+
+    public static class Duck {
+        public String toString() {
+            return "Duck";
+        }
+    }
+
+    //write your code here
+}
