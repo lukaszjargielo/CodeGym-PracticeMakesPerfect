54e878fa9524802181d1090b2de1cfa7b3f5edc4
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 10:43:25 +0200

    Task 133 (Level 10/Lesson 4/Task 2) - My solution

--- src/en/codegym/task/jdk13/task05/task0511/Dog.java
+++ src/en/codegym/task/jdk13/task05/task0511/Dog.java
@@ -5,7 +5,25 @@ Populate the Dog class
 */
 
 public class Dog {
-    //write your code here
+
+    private String name;
+    private int height;
+    private String color;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
 
     public static void main(String[] args) {
 
