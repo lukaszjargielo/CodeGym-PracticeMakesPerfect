2392a0d9e5d766014c0f6873959daa2ee92454a6
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:39:56 +0200

    Task 135 (Level 10/Lesson 4/Task 5) - My solution

--- src/en/codegym/task/jdk13/task05/task0514/Solution.java
+++ src/en/codegym/task/jdk13/task05/task0514/Solution.java
@@ -6,10 +6,17 @@ Initializing objects
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Person person = new Person();
+        person.initialize("Lucas", 37);
     }
 
     static class Person {
-        //write your code here
+        private String name;
+        private int age;
+
+        public void initialize(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
