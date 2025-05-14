4051b8e5997c818db0774cd93f0ee17458445e97
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 10:38:06 +0200

    Task 132 (Level 10/Lesson 4/Task 1) - My solution

--- src/en/codegym/task/jdk13/task05/task0509/Friend.java
+++ src/en/codegym/task/jdk13/task05/task0509/Friend.java
@@ -5,7 +5,25 @@ Populate the Friend class
 */
 
 public class Friend {
-    //write your code here
+
+    private String name;
+    private int age;
+    private char sex;
+
+    public void initialize(String name) {
+        this.name =  name;
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 
     public static void main(String[] args) {
 
