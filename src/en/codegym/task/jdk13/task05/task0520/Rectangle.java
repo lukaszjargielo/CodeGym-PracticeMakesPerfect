a4ae1d37140e5a8c1dae641257f96c11a270b324
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 16:40:33 +0200

    Task 140 (Level 10/Lesson 6/Task 3) - My solution

--- src/en/codegym/task/jdk13/task05/task0520/Rectangle.java
+++ src/en/codegym/task/jdk13/task05/task0520/Rectangle.java
@@ -6,7 +6,36 @@ Populate the Rectangle class
 
 
 public class Rectangle {
-    //write your code here
+    private int top;
+    private int left;
+    private int width;
+    private int height;
+
+    public Rectangle(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = width;
+    }
+
+    public Rectangle(int top, int left) {
+        this.top = top;
+        this.left = left;
+    }
+
+    public Rectangle(Rectangle anotherRectangle) {
+        this.top = anotherRectangle.top;
+        this.left = anotherRectangle.left;
+        this.width = anotherRectangle.width;
+        this.height = anotherRectangle.height;
+    }
 
     public static void main(String[] args) {
 
