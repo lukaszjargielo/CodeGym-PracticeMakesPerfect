54f16cf325015d3f3f1d4ba9603a188118270e96
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 18:46:02 +0200

    Task 147 (Level 10/Lesson 9/Task 3) - My solution

--- src/en/codegym/task/jdk13/task05/task0518/Dog.java
+++ src/en/codegym/task/jdk13/task05/task0518/Dog.java
@@ -6,7 +6,25 @@ Dog registration
 
 
 public class Dog {
-    //write your code here
+
+    private String name;
+    private int height;
+    private String color;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public Dog(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
 
     public static void main(String[] args) {
 
