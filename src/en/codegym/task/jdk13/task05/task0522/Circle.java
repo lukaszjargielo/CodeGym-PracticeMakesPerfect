46f04b5e83f3ac62e9747005505086965dc51d5b
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 17:03:21 +0200

    Task 144 (Level 10/Lesson 6/Task 7) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0522/Circle.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task05.task0522;
+
+/* 
+Max constructors
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double radius;
+
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
