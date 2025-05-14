fa2f3dbce67cc448957c844e616986a1386768a9
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 10:39:41 +0200

    Task 133 (Level 10/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0511/Dog.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0511;
+
+/* 
+Populate the Dog class
+*/
+
+public class Dog {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
