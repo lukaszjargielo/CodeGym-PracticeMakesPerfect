37440baa31ed6e9f48ecf3c2e426ba5caae5a44b
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 16:45:47 +0200

    Task 142 (Level 10/Lesson 6/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0517/Cat.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0517;
+
+/* 
+Creating cats
+*/
+
+public class Cat {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
