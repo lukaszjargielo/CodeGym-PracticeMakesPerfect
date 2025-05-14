919ee97f3794bae0dd616d6e86f4e90806eb67a8
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 10:33:44 +0200

    Task 132 (Level 10/Lesson 4/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0509/Friend.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0509;
+
+/* 
+Populate the Friend class
+*/
+
+public class Friend {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
