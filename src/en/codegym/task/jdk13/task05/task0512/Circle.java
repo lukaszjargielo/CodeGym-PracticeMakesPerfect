e3a646ff2ec141de8d643943dec07bbc482b50a4
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:41:55 +0200

    Task 136 (Level 10/Lesson 4/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0512/Circle.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0512;
+
+/* 
+Populate the Circle class
+*/
+
+public class Circle {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
