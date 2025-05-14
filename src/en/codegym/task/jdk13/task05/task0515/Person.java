cd3c78daee89024aa0cbb0bff5c78343caaaca6c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:48:21 +0200

    Task 137 (Level 10/Lesson 4/Task 7) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0515/Person.java
@@ -0,0 +1,32 @@
+package en.codegym.task.jdk13.task05.task0515;
+
+/* 
+Initializing objects
+*/
+
+public class Person {
+    String name;
+    char sex;
+    int money;
+    int weight;
+    double size;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, char sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public void initialize(String name, int money, char sex) {
+        this.name = name;
+        this.money = money;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
