996dd57cf3a3f9c112f330dcbbf4c556af06b112
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 18:31:47 +0200

    Task 146 (Level 10/Lesson 9/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0526/Solution.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0526;
+
+/* 
+Man and woman
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+
+    //write your code here
+}
