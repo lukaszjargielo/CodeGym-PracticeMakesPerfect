bac2eac63c6bed2903b2f25800ed38c344afb499
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 14:15:23 +0200

    Task 139 (Level 10/Lesson 6/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0524/Circle.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task05.task0524;
+
+/* 
+The basis of a wheel
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double r;
+
+    //write your code here
+    public static void main(String[] args) {
+
+    }
+}
