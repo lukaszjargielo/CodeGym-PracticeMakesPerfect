e207c5cb1078d28f8d7e74021bc3f139d089c8f7
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 11:36:49 +0200

    Task 135 (Level 10/Lesson 4/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0514/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task05.task0514;
+
+/* 
+Initializing objects
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+
+    static class Person {
+        //write your code here
+    }
+}
