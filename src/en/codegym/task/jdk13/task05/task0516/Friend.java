5b4ac15804492c8dcfffe2496800671b30c57606
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 14:12:43 +0200

    Task 138 (Level 10/Lesson 6/Task 1) - My solution

--- src/en/codegym/task/jdk13/task05/task0516/Friend.java
+++ src/en/codegym/task/jdk13/task05/task0516/Friend.java
@@ -5,7 +5,24 @@ You can't buy friends
 */
 
 public class Friend {
-    //write your code here
+    private String name;
+    private int age;
+    private char sex;
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public Friend(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 
     public static void main(String[] args) {
 
