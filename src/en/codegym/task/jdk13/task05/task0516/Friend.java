2a5abb215bc32fa8f898b7f28e4979ffde68f585
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-14 14:09:26 +0200

    Task 138 (Level 10/Lesson 6/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0516/Friend.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task05.task0516;
+
+/* 
+You can't buy friends
+*/
+
+public class Friend {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
