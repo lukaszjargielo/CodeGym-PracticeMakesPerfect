9ec23698aa067bcb12eadaaa951070b2914372b8
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-15 07:08:57 +0200

    Task 149 (Level 10/Lesson 9/Task 5) - My solution

--- src/en/codegym/task/jdk13/task05/task0528/Solution.java
+++ src/en/codegym/task/jdk13/task05/task0528/Solution.java
@@ -9,6 +9,26 @@ Display today's date
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM dd yyyy");
+        String today = formatter.format(date);
+        System.out.println(today);
     }
 }
+
+/*
+My alternative solution
+
+ LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("LL dd yyyy", new Locale("pl"));
+        String today2 = localDate.format(dateTimeFormatter);
+        System.out.println(today2);
+ */
+
+/*
+Recommended solution
+
+SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM dd yyyy");
+        System.out.println(simpleDateFormat.format(new Date()));
+ */
