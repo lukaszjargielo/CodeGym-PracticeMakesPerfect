a9e4f6730d565865a132740909f56bb535f3ae77
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-15 06:40:55 +0200

    Task 149 (Level 10/Lesson 9/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task05/task0528/Solution.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task05.task0528;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/* 
+Display today's date
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
