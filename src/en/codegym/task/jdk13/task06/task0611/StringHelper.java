6f2a36250136c9356eb707379aaec216fc2f7ac1
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:17:19 +0200

    Task 155 (Level 11/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0611/StringHelper.java
@@ -0,0 +1,23 @@
+package en.codegym.task.jdk13.task06.task0611;
+
+/* 
+StringHelper class
+*/
+
+public class StringHelper {
+    public static String multiply(String text) {
+        String result = "";
+        //write your code here
+        return result;
+    }
+
+    public static String multiply(String text, int count) {
+        String result = "";
+        //write your code here
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
