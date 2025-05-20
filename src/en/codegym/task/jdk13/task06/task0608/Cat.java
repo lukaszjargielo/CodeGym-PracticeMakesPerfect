eb3630a8b7c18a43012e3a32fea604d12aff0648
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:09:18 +0200

    Task 154 (Level 11/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0608/Cat.java
@@ -0,0 +1,27 @@
+package en.codegym.task.jdk13.task06.task0608;
+
+/* 
+Static methods: int getCatCount() and setCatCount(int)
+*/
+
+public class Cat {
+    private static int catCount = 0;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static int getCatCount() {
+        //write your code here
+
+    }
+
+    public static void setCatCount(int catCount) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
