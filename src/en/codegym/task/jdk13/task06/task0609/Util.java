6f8ef376aa13ed6b9149bf0605ded2a647687a03
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 18:34:11 +0200

    Task 159 (Level 11/Lesson 4/Task 7) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0609/Util.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task06.task0609;
+
+/* 
+Distance between two points
+*/
+
+public class Util {
+    public static double getDistance(int x1, int y1, int x2, int y2) {
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
