4acd92ac716d128e85421841ae21a9b02bdb7968
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:46:55 +0200

    Task 157 (Level 11/Lesson 4/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0610/ConsoleReader.java
@@ -0,0 +1,34 @@
+package en.codegym.task.jdk13.task06.task0610;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+ConsoleReader class
+*/
+
+public class ConsoleReader {
+    public static String readString() throws Exception {
+        //write your code here
+
+    }
+
+    public static int readInt() throws Exception {
+        //write your code here
+
+    }
+
+    public static double readDouble() throws Exception {
+        //write your code here
+
+    }
+
+    public static boolean readBoolean() throws Exception {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
