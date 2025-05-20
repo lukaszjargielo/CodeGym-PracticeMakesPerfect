e12d7c5f4fd451feeb705a17b478f4167e7a4c1a
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:55:11 +0200

    Task 158 (Level 11/Lesson 4/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0612/Calculator.java
@@ -0,0 +1,36 @@
+package en.codegym.task.jdk13.task06.task0612;
+
+/* 
+Calculator
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        //write your code here
+        return 0;
+    }
+
+    public static int minus(int a, int b) {
+        //write your code here
+        return 0;
+    }
+
+    public static int multiply(int a, int b) {
+        //write your code here
+        return 0;
+    }
+
+    public static double divide(int a, int b) {
+        //write your code here
+        return 0;
+    }
+
+    public static double percent(int a, int b) {
+        //write your code here
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
