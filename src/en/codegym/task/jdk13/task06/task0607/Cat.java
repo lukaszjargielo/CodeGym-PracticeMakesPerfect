ec3bbf73eeded0c068ab57eb025f3b0b2973ce4d
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:07:27 +0200

    Task 153 (Level 11/Lesson 4/Task 1) - My solution

--- src/en/codegym/task/jdk13/task06/task0607/Cat.java
+++ src/en/codegym/task/jdk13/task06/task0607/Cat.java
@@ -5,7 +5,11 @@ Class counter
 */
 
 public class Cat {
-    //write your code here
+    private static int catCount = 0;
+
+    public Cat() {
+        catCount++;
+    }
 
     public static void main(String[] args) {
 
