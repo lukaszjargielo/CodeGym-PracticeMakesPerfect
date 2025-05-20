c24d41fad39ab6be0dcc23a3c6d91e5fee20b9f1
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:04:28 +0200

    Task 153 (Level 11/Lesson 4/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task06/task0607/Cat.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task06.task0607;
+
+/* 
+Class counter
+*/
+
+public class Cat {
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
