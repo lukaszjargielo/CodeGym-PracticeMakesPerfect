ceedab3f0e90243b088668c31b1e67d851d999dc
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 17:53:45 +0200

    Task 157 (Level 11/Lesson 4/Task 5) - My solution

--- src/en/codegym/task/jdk13/task06/task0610/ConsoleReader.java
+++ src/en/codegym/task/jdk13/task06/task0610/ConsoleReader.java
@@ -9,23 +9,23 @@ ConsoleReader class
 
 public class ConsoleReader {
     public static String readString() throws Exception {
-        //write your code here
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
     }
 
     public static int readInt() throws Exception {
-        //write your code here
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
     }
 
     public static double readDouble() throws Exception {
-        //write your code here
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Double.parseDouble(reader.readLine());
     }
 
     public static boolean readBoolean() throws Exception {
-        //write your code here
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Boolean.parseBoolean(reader.readLine());
     }
 
     public static void main(String[] args) throws Exception {
