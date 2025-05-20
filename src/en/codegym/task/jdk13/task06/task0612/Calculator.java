cb58611527bb4af46a61076c221a44fe5d953849
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 18:00:51 +0200

    Task 158 (Level 11/Lesson 4/Task 6) - My solution

--- src/en/codegym/task/jdk13/task06/task0612/Calculator.java
+++ src/en/codegym/task/jdk13/task06/task0612/Calculator.java
@@ -6,28 +6,23 @@ Calculator
 
 public class Calculator {
     public static int plus(int a, int b) {
-        //write your code here
-        return 0;
+        return a + b;
     }
 
     public static int minus(int a, int b) {
-        //write your code here
-        return 0;
+        return a - b;
     }
 
     public static int multiply(int a, int b) {
-        //write your code here
-        return 0;
+        return a * b;
     }
 
     public static double divide(int a, int b) {
-        //write your code here
-        return 0;
+        return a / (b * 1.0);
     }
 
     public static double percent(int a, int b) {
-        //write your code here
-        return 0;
+        return (a * 1.0) * b / 100;
     }
 
     public static void main(String[] args) {
