4e9311a719f689ed3c3160cb3667d6c27ce41eaf
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-20 18:38:19 +0200

    Task 159 (Level 11/Lesson 4/Task 7) - My solution

--- src/en/codegym/task/jdk13/task06/task0609/Util.java
+++ src/en/codegym/task/jdk13/task06/task0609/Util.java
@@ -6,10 +6,20 @@ Distance between two points
 
 public class Util {
     public static double getDistance(int x1, int y1, int x2, int y2) {
-        //write your code here
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public static void main(String[] args) {
 
     }
 }
+/*
+Recommended solution
+
+    public static double getDistance(int x1, int y1, int x2, int y2) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+ */
