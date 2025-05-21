83369493046e8bf8c6518a0e8e70c01c3e0336dd
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-21 10:04:07 +0200

    Task 162 (Level 11/Lesson 5/Task 3) - My solution

--- src/en/codegym/task/pro/task11/task1112/Solution.java
+++ src/en/codegym/task/pro/task11/task1112/Solution.java
@@ -8,6 +8,6 @@ public class Solution {
     public static void main(String[] args) {
         String name = "Diego";
         int age = 35;
-        System.out.println("Name : "name" Age : "age);
+        System.out.println("Name : " + name + " Age : " + age);
     }
 }
