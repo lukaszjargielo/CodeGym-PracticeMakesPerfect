2dc96c808f4baeadbd8f9a9bdf6ca4e264ef07ba
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-21 11:13:13 +0200

    Task 166 (Level 11/Lesson 5/Task 7) - Requirements

---
+++ src/en/codegym/task/pro/task11/task1116/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.pro.task11.task1116;
+
+/* 
+Correct class
+*/
+
+class solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+}
+
+public class Cat {
+    String name = "Tom";
+}
