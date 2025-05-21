b9a69317664fbc00982c1bc18d18e703d67b79de
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-21 14:24:16 +0200

    Task 171 (Level 11/Lesson 6/Task 3) - My solution

--- src/en/codegym/task/pro/task11/task1121/Solution.java
+++ src/en/codegym/task/pro/task11/task1121/Solution.java
@@ -8,7 +8,7 @@ Forgotten initialization
 
 public class Solution {
     public static void main(String[] args) {
-        int[] array;
+        int[] array = new int[10];
         for (int i = 0; i < 10; i++) {
             array[i] = i;
         }
