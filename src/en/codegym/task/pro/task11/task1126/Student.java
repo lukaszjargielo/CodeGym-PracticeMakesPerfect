1a70375e446eeb6a085dba52ca1db9593ef278f4
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-22 09:02:19 +0200

    Task 176 (Level 11/Lesson 6/Task 8) - Requirements

---
+++ src/en/codegym/task/pro/task11/task1126/Student.java
@@ -0,0 +1,13 @@
+package en.codegym.task.pro.task11.task1126;
+
+/* 
+Student
+*/
+
+public class Student {
+    private String name;
+
+    public void Student(String name) {
+        name = name;
+    }
+}
