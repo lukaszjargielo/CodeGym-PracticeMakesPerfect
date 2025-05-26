3900804f713e3cd64724fffff73e4d03237cb720
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-26 15:12:42 +0200

    Task 182 (Level 12/Lesson 4/Task 4) - My solution

--- src/com/codegym/task/task10/task1001/Solution.java
+++ src/com/codegym/task/task10/task1001/Solution.java
@@ -8,10 +8,10 @@ Task No. 1 about integer type conversions
 public class Solution {
     public static void main(String[] args) {
         int a = 0;
-        int b = (byte) a + 46;
+        int b =  a + 46;
         byte c = (byte) (a * b);
-        double f = (char) 1234.15;
-        long d = (short) (a + f / c + b);
+        double f = 1234.15;
+        long d = (long) (a + f / c + b);
         System.out.println(d);
     }
 }
