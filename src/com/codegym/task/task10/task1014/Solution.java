dd90c2a56c69152a6d576c8928a35f8b56b4efab
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-26 20:02:29 +0200

    Task 189 (Level 12/Lesson 7/Task 5) - My solution

--- src/com/codegym/task/task10/task1014/Solution.java
+++ src/com/codegym/task/task10/task1014/Solution.java
@@ -7,8 +7,8 @@ Use the fewest number of static modifiers
 
 public class Solution {
     public int A = 5;
-    public int B = 5;
-    public int C = 5;
+    public static int B = 5;
+    public static int C = 5;
     public static int D = 5;
 
     public static void main(String[] args) {
