6fefebe2da8ba7bb4fd1e6b75a3f795edec8699c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-26 16:04:39 +0200

    Task 184 (Level 12/Lesson 4/Task 6) - My solution

--- src/com/codegym/task/task10/task1006/Solution.java
+++ src/com/codegym/task/task10/task1006/Solution.java
@@ -8,10 +8,10 @@ Task No. 6 about integer type conversions
 public class Solution {
     public static void main(String[] args) {
         double d = (short) 2.50256e2d;
-        char c = (short) 'd';
+        char c = 'd';
         short s = (short) 2.22;
-        int i = (short) 150000;
-        float f = (short) 0.50f;
+        int i = 150000;
+        float f = 0.50f;
         double result = f + (i / c) - (d * s) - 500e-3;
         System.out.println("result: " + result);
     }
