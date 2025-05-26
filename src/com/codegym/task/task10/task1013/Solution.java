3f7cdb9deb56982d5756c352b461d9200a688958
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-26 18:49:13 +0200

    Task 188 (Level 12/Lesson 7/Task 4) - Requirements

---
+++ src/com/codegym/task/task10/task1013/Solution.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task10.task1013;
+
+/* 
+Human class constructors
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static class Human {
+        // Write your variables and constructors here
+    }
+}
