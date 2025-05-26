70b56af341535eda478e7e2750e7b8a083df0e28
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-26 18:20:36 +0200

    Task 186 (Level 12/Lesson 7/Task 2) - My solution

--- src/en/codegym/task/jdk13/task12/task1204/Solution.java
+++ src/en/codegym/task/jdk13/task12/task1204/Solution.java
@@ -14,6 +14,6 @@ public class Solution {
     }
 
     public static String add(int a, int b) {
-        return a + '+' + b + '=' + (a + b) + "";
+        return a + "+" + b + "=" + (a + b);
     }
 }
