c4a2933635a2dd5412bb28281323f3c006fd393f
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-27 18:00:30 +0200

    Task 192 (Level 13/Lesson 2/Task 2) - My solution

--- src/en/codegym/task/jdk13/task07/task0710/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0710/Solution.java
@@ -10,6 +10,15 @@ To the top of the list
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        ArrayList<String> strings = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            strings.add(0, reader.readLine());
+        }
+
+        for (var element : strings) {
+            System.out.println(element);
+        }
     }
 }
