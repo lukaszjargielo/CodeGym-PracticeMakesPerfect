e22f45ea38f5a4e3b80d25c5042d945632b472c0
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-27 17:49:15 +0200

    Task 191 (Level 13/Lesson 2/Task 1) - My solution

--- src/en/codegym/task/jdk13/task07/task0707/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0707/Solution.java
@@ -1,6 +1,7 @@
 package en.codegym.task.jdk13.task07.task0707;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 
 5 different strings in a list
@@ -11,6 +12,10 @@ public class Solution {
     public static ArrayList<String> list;
 
     public static void main(String[] args) {
-        //write your code here
+        list = new ArrayList<String>(Arrays.asList("String1", "String2", "String2", "String4", "String5"));
+        System.out.println(list.size());
+        for (var element : list) {
+            System.out.println(element);
+        }
     }
 }
