b5a79dc1476a1de344d1dacd471dc774e13e6475
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-27 17:42:58 +0200

    Task 191 (Level 13/Lesson 2/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0707/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task07.task0707;
+
+import java.util.ArrayList;
+
+/* 
+5 different strings in a list
+*/
+
+public class Solution {
+
+    public static ArrayList<String> list;
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
