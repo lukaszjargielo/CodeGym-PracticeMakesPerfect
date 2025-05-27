9b43b4b2e437b7b0e4928b2a4b361b3abec643ad
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-27 18:14:20 +0200

    Task 193 (Level 13/Lesson 2/Task 3) - My solution

--- src/en/codegym/task/jdk13/task07/task0708/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0708/Solution.java
@@ -3,6 +3,8 @@ package en.codegym.task.jdk13.task07.task0708;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /* 
 The largest number
@@ -13,6 +15,30 @@ public class Solution {
     private static ArrayList<Integer> integers = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            integers.add(Integer.parseInt(reader.readLine()));
+        }
+
+        Collections.sort(integers);
+        System.out.println(integers.get(integers.size()-1));
     }
 }
+
+/*
+Recommended solution
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            integers.add(Integer.parseInt(reader.readLine()));
+        }
+
+        Integer[] array = integers.toArray(new Integer[0]);
+        Arrays.sort(array);
+
+        System.out.println(array[array.length - 1]);
+    }
+}
+ */
