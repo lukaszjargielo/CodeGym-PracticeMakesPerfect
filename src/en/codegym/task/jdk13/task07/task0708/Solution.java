f5f6d5f09c15e37ed0b38b87c27283f02eba8e6e
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-27 18:02:27 +0200

    Task 193 (Level 13/Lesson 2/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0708/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task07.task0708;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+The largest number
+*/
+
+public class Solution {
+
+    private static ArrayList<Integer> integers = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+}
