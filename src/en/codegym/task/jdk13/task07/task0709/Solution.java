7bb0ab10179d773cd427352493018e58fc2d5686
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-27 18:18:30 +0200

    Task 194 (Level 13/Lesson 2/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0709/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task07.task0709;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Expressing ourselves more concisely
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+}
