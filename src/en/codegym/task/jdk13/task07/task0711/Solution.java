b8d5e9efa6c4efe4eecf1685a159af53845f9f43
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-28 12:48:36 +0200

    Task 196 (Level 13/Lesson 2/Task 6) - My solution

--- src/en/codegym/task/jdk13/task07/task0711/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0711/Solution.java
@@ -10,6 +10,36 @@ Remove and insert
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        ArrayList<String> strings = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            strings.add(reader.readLine());
+        }
+
+        for (int i = 0; i < 13; i++) {
+            String removedString = strings.remove(4);
+            strings.add(0, removedString);
+        }
+
+        for (var element : strings) {
+            System.out.println(element);
+        }
     }
 }
+
+/*
+My solution which is not compiled on the server
+
+   for (int i = 0; i < 13; i++) {
+            String removedString = strings.removeFirst();
+            strings.addFirst(removedString);
+        }
+
+Recommended solution
+
+ for (int i = 0; i < 13; i++) {
+            String string = strings.remove(4);
+            strings.add(0, string);
+        }
+ */
