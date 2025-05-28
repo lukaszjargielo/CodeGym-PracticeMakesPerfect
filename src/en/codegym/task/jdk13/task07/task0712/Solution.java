e267ea1b8e7c239742cfc37d5f6fbbcbcb8ad36e
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-28 11:01:01 +0200

    Task 195 (Level 13/Lesson 2/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0712/Solution.java
@@ -0,0 +1,19 @@
+package en.codegym.task.jdk13.task07.task0712;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Minimum or maximum
+*/
+
+public class Solution {
+
+    public static ArrayList<String> strings;
+
+    public static void main(String[] args) throws IOException {
+        //write your code here
+    }
+}
