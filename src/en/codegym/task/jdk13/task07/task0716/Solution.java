2da48a6196eb29ebb80e5edfae84e2fc13287def
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-28 17:23:39 +0200

    Task 199 (Level 13/Lesson 5/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0716/Solution.java
@@ -0,0 +1,26 @@
+package en.codegym.task.jdk13.task07.task0716;
+
+import java.util.ArrayList;
+
+/* 
+R or L
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("rose");
+        strings.add("love");
+        strings.add("lyra");
+        strings = fix(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> strings) {
+        //write your code here
+        return null;
+    }
+}
