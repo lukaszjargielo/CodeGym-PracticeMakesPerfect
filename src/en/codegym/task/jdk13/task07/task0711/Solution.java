6460b7a04968430509a1dd6772baba02b5abd954
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-28 12:26:28 +0200

    Task 196 (Level 13/Lesson 2/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0711/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task07.task0711;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Remove and insert
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+}
