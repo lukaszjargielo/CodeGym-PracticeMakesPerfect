c66526a7c10a79d04d00ab550747225dc7fb10f3
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-28 17:01:01 +0200

    Task 198 (Level 13/Lesson 5/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0713/Solution.java
@@ -0,0 +1,25 @@
+package en.codegym.task.jdk13.task07.task0713;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Three arrays
+*/
+
+public class Solution {
+
+    public static ArrayList<Integer> numbers = new ArrayList<>();
+    public static ArrayList<Integer> divBy3 = new ArrayList<>();
+    public static ArrayList<Integer> divBy2 = new ArrayList<>();
+    public static ArrayList<Integer> others = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+
+    public static void printList(ArrayList<Integer> list) {
+        //write your code here
+    }
+}
