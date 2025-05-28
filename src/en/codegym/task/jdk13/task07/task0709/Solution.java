35345c2c516c6f101f4f3052bc27f29740980776
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-28 10:59:27 +0200

    Task 194 (Level 13/Lesson 2/Task 4) - My solution

--- src/en/codegym/task/jdk13/task07/task0709/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0709/Solution.java
@@ -10,6 +10,42 @@ Expressing ourselves more concisely
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        ArrayList<String> strings = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            strings.add(reader.readLine());
+        }
+
+        int minLength = strings.get(0).length();
+
+        for (int i = 0; i < strings.size(); i++) {
+            if (strings.get(i).length() < minLength) {
+                minLength = strings.get(i).length();
+            }
+        }
+
+        for (var element : strings) {
+            if (element.length() == minLength) {
+                System.out.println(element);
+            }
+        }
     }
 }
+
+/*
+Recommended solution
+
+ int minStringLength = strings.get(0).length();
+        for (String string : strings) {
+            if (string.length() < minStringLength) {
+                minStringLength = string.length();
+            }
+        }
+
+        for (String string : strings) {
+            if (string.length() == minStringLength) {
+                System.out.println(string);
+            }
+        }
+ */
