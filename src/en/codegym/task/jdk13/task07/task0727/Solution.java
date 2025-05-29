fd9477c4e7bf73fe84e03a749a1dd37cb9f43510
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 15:19:54 +0200

    Task 206 (Level 13/Lesson 7/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0727/Solution.java
@@ -0,0 +1,32 @@
+package en.codegym.task.jdk13.task07.task0727;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+You need to replace the program's functionality
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<String>();
+        while (true) {
+            String string = reader.readLine();
+            if (string == null || string.isEmpty()) break;
+            strings.add(string);
+        }
+
+        ArrayList<String> resultStrings = new ArrayList<String>();
+        for (String string : strings) {
+            resultStrings.add(string.toUpperCase());
+        }
+
+        for (String resultString : resultStrings) {
+            System.out.println(resultString);
+        }
+    }
+}
