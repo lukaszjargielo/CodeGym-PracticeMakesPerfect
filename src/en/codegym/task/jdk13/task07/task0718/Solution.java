d2224945ce6ac2b6f05384b8645d7640046d07fd
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 11:23:59 +0200

    Task 202 (Level 13/Lesson 5/Task 6) - My solution

--- src/en/codegym/task/jdk13/task07/task0718/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0718/Solution.java
@@ -11,7 +11,38 @@ Checking the order
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            strings.add(reader.readLine());
+        }
+
+        int comparedLength = strings.get(0).length();
+
+        for (int i = 1; i < strings.size(); i++) {
+            if (strings.get(i).length() < comparedLength) {
+                System.out.println(i);
+                break;
+            } else {
+                comparedLength = strings.get(i).length();
+            }
+        }
     }
 }
 
+/*
+Recommended solution
+
+        String currentString = strings.get(0);
+
+        for (int i = 1; i < 10; i++) {
+            if (strings.get(i).length() >= currentString.length()) {
+                currentString = strings.get(i);
+            } else {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+ */
