1d98fb59080d02a37a1f3fcd94a808499085898e
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 10:41:07 +0200

    Task 200 (Level 13/Lesson 5/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0715/Solution.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task07.task0715;
+
+import java.util.ArrayList;
+
+/* 
+More Sam-I-Am
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
