249d498edc96ca5b9beb313cdf2911f67343148c
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 10:49:42 +0200

    Task 200 (Level 13/Lesson 5/Task 4) - My solution

--- src/en/codegym/task/jdk13/task07/task0715/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0715/Solution.java
@@ -1,6 +1,7 @@
 package en.codegym.task.jdk13.task07.task0715;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 
 More Sam-I-Am
@@ -8,6 +9,34 @@ More Sam-I-Am
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        ArrayList<String> strings = new ArrayList<>();
+        strings.addAll(Arrays.asList("Sam", "I", "Am"));
+
+        for (int i = 1; i <= strings.size(); i+=2) {
+            strings.add(i,"Ham");
+        }
+
+        for (var element : strings){
+            System.out.println(element);
+        }
+
     }
 }
+
+/*
+Recommended solution
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Sam");
+        list.add("I");
+        list.add("Am");
+
+        list.add(1, "Ham");
+        list.add(3, "Ham");
+        list.add(5, "Ham");
+
+        for (String string : list) {
+            System.out.println(string);
+        }
+    }
+ */
