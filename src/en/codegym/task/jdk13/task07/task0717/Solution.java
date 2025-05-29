4576179e75094855ff0facde35815d69a91fd8c0
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 10:52:37 +0200

    Task 201 (Level 13/Lesson 5/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0717/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.jdk13.task07.task0717;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Duplicating words
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Read strings from the console and declare an ArrayList here
+
+        ArrayList<String> result = doubleValues(list);
+
+        // Display result
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        //write your code here
+        return null;
+    }
+}
