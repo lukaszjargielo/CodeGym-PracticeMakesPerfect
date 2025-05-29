d234e3a1c45f2b7fbcc75655af0571cf2a32bfdb
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 15:02:36 +0200

    Task 205 (Level 13/Lesson 7/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0720/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task07.task0720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Shuffled just in time
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //write your code here
+    }
+}
