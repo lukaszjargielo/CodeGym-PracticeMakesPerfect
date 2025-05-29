d3941548085eb8df9b033061c86a7fe1fa5cbeef
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 15:17:32 +0200

    Task 205 (Level 13/Lesson 7/Task 3) - My solution

--- src/en/codegym/task/jdk13/task07/task0720/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0720/Solution.java
@@ -12,7 +12,28 @@ Shuffled just in time
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
 
-        //write your code here
+        int N = Integer.parseInt(reader.readLine());
+        int M = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < N; i++) {
+            list.add(reader.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String s = list.remove(0);
+            list.add(s);
+        }
+
+        list.forEach(System.out::println);
     }
 }
+
+/*
+Recommended solution
+
+ for (int i = 0; i < m; i++) {
+            strings.add(strings.remove(0));
+        }
+ */
