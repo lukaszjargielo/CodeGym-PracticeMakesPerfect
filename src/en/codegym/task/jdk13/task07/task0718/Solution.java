74d060e7be6adc36612c635c3a9cbc270cd7ae7b
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 11:05:48 +0200

    Task 202 (Level 13/Lesson 5/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0718/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task07.task0718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Checking the order
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //write your code here
+    }
+}
+
