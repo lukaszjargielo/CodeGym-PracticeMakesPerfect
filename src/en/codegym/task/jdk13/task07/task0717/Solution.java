f4071ceb2b2c3cbafb83203219bb12095b316e30
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 11:01:54 +0200

    Task 201 (Level 13/Lesson 5/Task 5) - My solution

--- src/en/codegym/task/jdk13/task07/task0717/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0717/Solution.java
@@ -10,15 +10,35 @@ Duplicating words
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        // Read strings from the console and declare an ArrayList here
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
 
         ArrayList<String> result = doubleValues(list);
 
-        // Display result
+      result.forEach(System.out::println);
     }
 
     public static ArrayList<String> doubleValues(ArrayList<String> list) {
-        //write your code here
-        return null;
+        for (int i = 0; i < list.size(); i+=2) {
+            list.add(i,list.get(i));
+        }
+        return list;
     }
 }
+
+/*
+Recommended solution
+
+public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String string : list) {
+            result.add(string);
+            result.add(string);
+        }
+        return result;
+    }
+ */
