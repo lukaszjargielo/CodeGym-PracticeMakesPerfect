50081146cfcdc183b7c04cae8a3acb8760702326
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 14:59:42 +0200

    Task 204 (Level 13/Lesson 7/Task 2) - My solution

--- src/en/codegym/task/jdk13/task07/task0719/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0719/Solution.java
@@ -12,7 +12,14 @@ Display numbers in reverse order
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        //write your code here
+        for (int i = 0; i < 10; i++) {
+            numbers.add(Integer.parseInt(reader.readLine()));
+        }
+
+        for (int i = numbers.size() - 1; i >= 0 ; i--) {
+            System.out.println(numbers.get(i));
+        }
     }
 }
