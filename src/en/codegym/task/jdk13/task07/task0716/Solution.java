6d6d3e219367a21e9242811b9c91f23803e92f42
lukaszjargielo <l.jargielo88@gmail.com>
2025-05-29 09:49:56 +0200

    Task 199 (Level 13/Lesson 5/Task 3) - My solution

--- src/en/codegym/task/jdk13/task07/task0716/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0716/Solution.java
@@ -2,7 +2,7 @@ package en.codegym.task.jdk13.task07.task0716;
 
 import java.util.ArrayList;
 
-/* 
+/*
 R or L
 */
 
@@ -20,7 +20,44 @@ public class Solution {
     }
 
     public static ArrayList<String> fix(ArrayList<String> strings) {
-        //write your code here
-        return null;
+        for (int i = 0; i < strings.size(); ) {
+            if (strings.get(i).contains("r") && !strings.get(i).contains("l")) {
+                strings.remove(strings.get(i));
+            } else if (strings.get(i).contains("l") && !strings.get(i).contains("r")) {
+                strings.add(0, strings.get(i));
+                i+=2;
+            } else {
+                i++;
+            }
+        }
+        return strings;
     }
 }
+
+/*
+Recommended solution
+
+    public static ArrayList<String> fix(ArrayList<String> strings) {
+
+        String r = "r";
+        String l = "l";
+
+        ArrayList<String> result = new ArrayList<>();
+        for (String string : strings) {
+            boolean isR = string.contains(r);
+            boolean isL = string.contains(l);
+
+            if (isR && !isL) {
+                continue;
+            }
+
+            if (!isR && isL) {
+                result.add(string);
+            }
+
+            result.add(string);
+        }
+        return result;
+    }
+}
+ */
