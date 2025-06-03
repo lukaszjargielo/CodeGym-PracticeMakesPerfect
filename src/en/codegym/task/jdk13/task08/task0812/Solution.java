3ab4397b4e396d573d3a97df5bc08308c6e4bbbd
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 17:26:29 +0200

    Task 221 (Level 14/Lesson 5/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0812/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task08.task0812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Longest sequence
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //write your code here
+
+    }
+}
