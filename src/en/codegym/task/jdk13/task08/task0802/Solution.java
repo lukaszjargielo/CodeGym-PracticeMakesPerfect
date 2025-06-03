f7351c5c1a12c2fdd1e136eeddeaee1077085aa7
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 11:33:12 +0200

    Task 211 (Level 14/Lesson 3/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0802/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task08.task0802;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Map of 10 pairs
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
