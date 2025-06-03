5aaef286ed6b0560850bcee154022416486e78bb
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 11:31:11 +0200

    Task 210 (Level 14/Lesson 3/Task 1) - My solution

--- src/en/codegym/task/jdk13/task08/task0801/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0801/Solution.java
@@ -9,7 +9,23 @@ Set of plants
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Set<String> plants = new HashSet<>();
+
+        plants.add("watermelon");
+        plants.add("banana");
+        plants.add("cherry");
+        plants.add("pear");
+        plants.add("cantaloupe");
+        plants.add("blackberry");
+        plants.add("ginseng");
+        plants.add("strawberry");
+        plants.add("iris");
+        plants.add("potato");
+
+        for (String plant : plants) {
+            System.out.println(plant);
+        }
+
 
     }
 }
