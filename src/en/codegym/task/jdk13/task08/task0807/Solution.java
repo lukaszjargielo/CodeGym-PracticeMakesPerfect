0c6e441211668f2910bae5a92446412097275c77
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 15:02:02 +0200

    Task 216 (Level 14/Lesson 5/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0807/Solution.java
@@ -0,0 +1,24 @@
+package en.codegym.task.jdk13.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 
+LinkedList and ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //write your code here
+
+    }
+
+    public static Object createLinkedList() {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
