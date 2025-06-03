9f94ffdef145f944f3ace68f78df3c8e2e866988
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 17:25:00 +0200

    Task 220 (Level 14/Lesson 5/Task 5) - My solution

--- src/en/codegym/task/jdk13/task08/task0811/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0811/Solution.java
@@ -10,23 +10,19 @@ Method quartet
 
 public class Solution {
     public static List getListForGet() {
-        //write your code here
-
+        return new ArrayList<>();
     }
 
     public static List getListForSet() {
-        //write your code here
-
+        return new ArrayList<>();
     }
 
     public static List getListForAddOrInsert() {
-        //write your code here
-
+        return new LinkedList();
     }
 
     public static List getListForRemove() {
-        //write your code here
-
+        return new LinkedList();
     }
 
     public static void main(String[] args) {
