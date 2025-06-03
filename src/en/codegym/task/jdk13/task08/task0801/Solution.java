ff4ae9be00acdf7d28b2c7630b21c31f276fa5c9
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 11:25:25 +0200

    Task 210 (Level 14/Lesson 3/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0801/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task08.task0801;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Set of plants
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+}
