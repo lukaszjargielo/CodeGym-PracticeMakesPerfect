e1c491a9110722c82f1a56580262597db4458d96
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 18:06:19 +0200

    Task 221 (Level 14/Lesson 5/Task 6) - My solution

--- src/en/codegym/task/jdk13/task08/task0812/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0812/Solution.java
@@ -12,7 +12,49 @@ Longest sequence
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> numbers = new ArrayList<>();
 
+        for (int i = 0; i < 10; i++) {
+            numbers.add(Integer.parseInt(reader.readLine()));
+        }
+
+        int repsCounter = 1;
+        int maxRepsCounter = 1;
+
+        for (int i = 0; i < 9; i++) {
+            if (numbers.get(i).equals(numbers.get(i + 1))) {
+                repsCounter++;
+                if (repsCounter > maxRepsCounter) {
+                    maxRepsCounter = repsCounter;
+                }
+            } else {
+                repsCounter = 1;
+            }
+        }
+
+        System.out.println(maxRepsCounter);
     }
 }
+
+/*
+Recommended solution
+
+        int count = 1;
+        int max = 1;
+
+        for (int i = 0; i < 9; i++) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                max++;
+            } else if (max > count) {
+                count = max;
+                max = 1;
+            } else {
+                max = 1;
+            }
+        }
+
+        if (max > count) {
+            count = max;
+        }
+ */
