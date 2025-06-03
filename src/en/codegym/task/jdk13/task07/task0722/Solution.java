ad19ac8e20ad6bc1e8ad6547520949499b290b17
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 07:52:09 +0200

    Task 207 (Level 13/Lesson 7/Task 5) - My solution

--- src/en/codegym/task/jdk13/task07/task0722/Solution.java
+++ src/en/codegym/task/jdk13/task07/task0722/Solution.java
@@ -3,6 +3,7 @@ package en.codegym.task.jdk13.task07.task0722;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.ArrayList;
 
 /* 
@@ -12,7 +13,16 @@ The end
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> strings = new ArrayList<>();
 
-        //write your code here
+        while (true) {
+            String input = reader.readLine();
+            if(input.equals("end")) {
+                break;
+            }
+            strings.add(input);
+        }
+
+        strings.forEach(System.out::println);
     }
 }
