efb61131849b5fb43297e22baa4ef0d77922a237
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 07:54:12 +0200

    Task 208 (Level 13/Lesson 7/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task07/task0721/Solution.java
@@ -0,0 +1,22 @@
+package en.codegym.task.jdk13.task07.task0721;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Maximum and minimum numbers in an array
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int maximum;
+        int minimum;
+
+        //write your code here
+
+        System.out.print(maximum + " " + minimum);
+    }
+}
