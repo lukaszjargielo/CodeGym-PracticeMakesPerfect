5c4f97e5b197173b7383f3e7d94e9acb4d78498a
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-03 11:41:49 +0200

    Task 211 (Level 14/Lesson 3/Task 2) - My solution

--- src/en/codegym/task/jdk13/task08/task0802/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0802/Solution.java
@@ -9,7 +9,31 @@ Map of 10 pairs
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Map<String, String> kindsOfPlants = new HashMap<>();
 
+        kindsOfPlants.put("watermelon", "melon");
+        kindsOfPlants.put("banana", "fruit");
+        kindsOfPlants.put("cherry", "fruit");
+        kindsOfPlants.put("pear", "fruit");
+        kindsOfPlants.put("cantaloupe", "melon");
+        kindsOfPlants.put("blackberry", "fruit");
+        kindsOfPlants.put("ginseng", "root");
+        kindsOfPlants.put("strawberry", "fruit");
+        kindsOfPlants.put("iris", "flower");
+        kindsOfPlants.put("potato", "tuber");
+
+        for(Map.Entry<String, String> entry : kindsOfPlants.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " - " + value);
+        }
     }
 }
+
+/*
+Recommended solution
+
+for (Map.Entry<String, String> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+ */
