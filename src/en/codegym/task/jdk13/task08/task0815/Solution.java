76fdfd68b1854ec157e80d65b35948598282e8dc
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-04 10:29:57 +0200

    Task 224 (Level 14/Lesson 7/Task 3) - My solution

--- src/en/codegym/task/jdk13/task08/task0815/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0815/Solution.java
@@ -1,7 +1,9 @@
 package en.codegym.task.jdk13.task08.task0815;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /* 
 Census
@@ -9,21 +11,95 @@ Census
 
 public class Solution {
     public static Map<String, String> createMap() {
-        //write your code here
+        Map<String, String> surnameAndName = new HashMap<>();
 
+        Map<String, String> source = Map.of(
+                "Smith", "James",
+                "Johnson", "Emily",
+                "Brown", "David",
+                "Davis", "Sarah",
+                "Miller", "William",
+                "Wilson", "Jessica",
+                "Moore", "David",
+                "Taylor", "Emma",
+                "Anderson", "John",
+                "Thomas", "Olivia");
+
+        surnameAndName.putAll(source);
+        return surnameAndName;
     }
 
     public static int getSameFirstNameCount(Map<String, String> map, String name) {
-        //write your code here
+        int counter = 0;
 
+        Collection<String> firstNames = map.values();
+        for (String firstName : firstNames) {
+            if (firstName.equals(name)) {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 
     public static int getSameLastNameCount(Map<String, String> map, String lastName) {
-        //write your code here
+        int counter = 0;
+
+        Set<String> lastNames = map.keySet();
+        for (String surname : lastNames) {
+            if (surname.equals(lastName)) {
+                counter++;
+            }
+        }
+
+        return counter;
 
     }
 
     public static void main(String[] args) {
-
+       /* Map<String, String> map = createMap();
+        System.out.println(getSameFirstNameCount(map, "David"));
+        System.out.println(getSameLastNameCount(map, "Miller"));*/
     }
 }
+
+/*
+Recommended solution
+
+ public static Map<String, String> createMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Smith", "Sophia");
+        map.put("Johnson", "Isabella");
+        map.put("Williams", "Jacob");
+        map.put("Brown", "Mason");
+        map.put("Jones", "William");
+        map.put("Garcia", "Jayden");
+        map.put("Miller", "Emily");
+        map.put("Davis", "Abigail");
+        map.put("Rodriguez", "Mia");
+        map.put("Martinez", "Joshua");
+        return map;
+    }
+
+    public static int getSameFirstNameCount(Map<String, String> map, String name) {
+        int count = 0;
+        for (String nameTmp : map.values()) {
+            if (nameTmp.equals(name)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int getSameLastNameCount(Map<String, String> map, String lastName) {
+        int count = 0;
+        for (String lastNameTmp : map.keySet()) {
+            if (lastNameTmp.equals(lastName)) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+ */
