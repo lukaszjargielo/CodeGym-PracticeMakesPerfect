4a3a2c0b5d7682bcc09bdd04695873676fe12db0
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-04 09:51:00 +0200

    Task 224 (Level 14/Lesson 7/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0815/Solution.java
@@ -0,0 +1,29 @@
+package en.codegym.task.jdk13.task08.task0815;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Census
+*/
+
+public class Solution {
+    public static Map<String, String> createMap() {
+        //write your code here
+
+    }
+
+    public static int getSameFirstNameCount(Map<String, String> map, String name) {
+        //write your code here
+
+    }
+
+    public static int getSameLastNameCount(Map<String, String> map, String lastName) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
