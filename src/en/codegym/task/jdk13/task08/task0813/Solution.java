d953a42bb98605c0bfb48e820a21de78d88d963e
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-04 09:20:21 +0200

    Task 222 (Level 14/Lesson 7/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0813/Solution.java
@@ -0,0 +1,19 @@
+package en.codegym.task.jdk13.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 words that start with the letter "L"
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
