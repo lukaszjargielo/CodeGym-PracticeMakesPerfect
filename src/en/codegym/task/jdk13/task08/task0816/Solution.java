d6b8ebb75b3ee09fef26748b5e8533b60dc638ce
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-05 11:07:12 +0200

    Task 226 (Level 14/Lesson 7/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0816/Solution.java
@@ -0,0 +1,32 @@
+package en.codegym.task.jdk13.task08.task0816;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/* 
+Remove all people born in the summer
+*/
+
+public class Solution {
+    public static Map<String, Date> createMap() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+        Map<String, Date> map = new HashMap<>();
+        map.put("Smith", dateFormat.parse("MAY 1 2012"));
+
+        //write your code here
+    }
+
+    public static void removeAllSummerPeople(Map<String, Date> map) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
