7dcef32ff4ac5243f3c3cce77ce1e3b76aab77aa
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-05 11:53:38 +0200

    Task 227 (Level 14/Lesson 7/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0818/Solution.java
@@ -0,0 +1,22 @@
+package en.codegym.task.jdk13.task08.task0818;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Only for the rich
+*/
+
+public class Solution {
+    public static Map<String, Integer> createMap() {
+        //write your code here
+    }
+
+    public static void removeItemFromMap(Map<String, Integer> map) {
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
