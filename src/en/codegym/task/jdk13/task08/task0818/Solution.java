116e7874bf23a0c13f4255393a1a00ef94f3ac6c
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-05 15:31:12 +0200

    Task 227 (Level 14/Lesson 7/Task 6) - My solution

--- src/en/codegym/task/jdk13/task08/task0818/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0818/Solution.java
@@ -1,6 +1,7 @@
 package en.codegym.task.jdk13.task08.task0818;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /* 
@@ -9,14 +10,47 @@ Only for the rich
 
 public class Solution {
     public static Map<String, Integer> createMap() {
-        //write your code here
+        Map<String, Integer> nameAndSalary = new HashMap<>(Map.of(
+                "Taylor", 600,
+                "Johnson", 800,
+                "Miller", 400,
+                "Wilson", 300,
+                "Moore", 1000,
+                "Anderson", 100,
+                "Thomas", 200,
+                "Brown", 700,
+                "Davis", 800,
+                "Smith", 1000
+        ));
+
+        return nameAndSalary;
     }
 
     public static void removeItemFromMap(Map<String, Integer> map) {
-        //write your code here
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getValue() < 500) {
+                iterator.remove();
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+/*        Map<String, Integer> map = createMap();
+        System.out.println(map);
+        removeItemFromMap(map);
+        System.out.println(map);*/
     }
 }
+/*
+Recommended solution
+
+public static void removeItemFromMap(Map<String, Integer> map) {
+        Map<String, Integer> copy = new HashMap<>(map);
+        for (Map.Entry<String, Integer> pair : copy.entrySet()) {
+            if (pair.getValue() < 500) {
+                map.remove(pair.getKey());
+            }
+        }
+    }
+*/
