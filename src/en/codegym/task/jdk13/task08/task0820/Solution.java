b546b23b4a5207f6cde4aac436c1a8c8acac4044
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-06 16:44:22 +0200

    Task 229 (Level 14/Lesson 9/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0820/Solution.java
@@ -0,0 +1,49 @@
+package en.codegym.task.jdk13.task08.task0820;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Animal set
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<Cat>();
+
+        //write your code here
+
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        //write your code here
+        return null;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        //write your code here
+        return null;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        //write your code here
+    }
+
+    public static void printPets(Set<Object> pets) {
+        //write your code here
+    }
+
+    //write your code here
+}
