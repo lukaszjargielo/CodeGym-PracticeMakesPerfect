6263c50e1baae84a7638a1c0c5b47be7fb978f7c
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-06 16:59:33 +0200

    Task 230 (Level 14/Lesson 9/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0821/Solution.java
@@ -0,0 +1,27 @@
+package en.codegym.task.jdk13.task08.task0821;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Shared last names and first names
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleMap();
+        printPeopleMap(map);
+    }
+
+    public static Map<String, String> createPeopleMap() {
+        //write your code here
+
+        return null;
+    }
+
+    public static void printPeopleMap(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
