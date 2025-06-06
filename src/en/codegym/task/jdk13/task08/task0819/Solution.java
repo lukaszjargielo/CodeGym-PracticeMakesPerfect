334315b23a8fc8de9346ce5e46cdbb25e658b43a
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-06 16:16:50 +0200

    Task 228 (Level 14/Lesson 9/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0819/Solution.java
@@ -0,0 +1,29 @@
+package en.codegym.task.jdk13.task08.task0819;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Set of cats
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+
+        //write your code here. step 3
+
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        //write your code here. step 2
+        return null;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        // step 4
+    }
+
+    // step 1
+}
