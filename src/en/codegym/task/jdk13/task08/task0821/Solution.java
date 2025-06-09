3483420a53a6886db86fa89f771d6e6a1cee41af
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-09 09:03:15 +0200

    Task 230 (Level 14/Lesson 9/Task 3) - My solution

--- src/en/codegym/task/jdk13/task08/task0821/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0821/Solution.java
@@ -1,3 +1,5 @@
+
+
 package en.codegym.task.jdk13.task08.task0821;
 
 import java.util.HashMap;
@@ -10,13 +12,24 @@ Shared last names and first names
 public class Solution {
     public static void main(String[] args) {
         Map<String, String> map = createPeopleMap();
+
         printPeopleMap(map);
     }
 
     public static Map<String, String> createPeopleMap() {
-        //write your code here
+        Map<String, String> surnameAndName = new HashMap<>();
+        surnameAndName.put("Smith", "James");
+        surnameAndName.put("Johnson", "Emily");
+        surnameAndName.put("Brown", "David");
+        surnameAndName.put("Davis", "Sarah");
+        surnameAndName.put("Miller", "William");
+        surnameAndName.put("Wilson", "Jessica");
+        surnameAndName.put("Moore", "David");
+        surnameAndName.put("Miller", "Emma");
+        surnameAndName.put("Anderson", "John");
+        surnameAndName.put("Thomas", "Olivia");
 
-        return null;
+        return surnameAndName;
     }
 
     public static void printPeopleMap(Map<String, String> map) {
@@ -25,3 +38,23 @@ public class Solution {
         }
     }
 }
+
+//ATTENTION TRICKY REQUIREMENTS!!!
+//We really shall add the same keys!!!
+
+/*
+Wrong way
+
+Map<String, String> surnameAndName = new HashMap<>(Map.of(
+                "Smith", "James",
+                "Johnson", "Emily",
+                "Brown", "David",
+                "Davis", "Sarah",
+                "Miller", "William",
+                "Wilson", "Jessica",
+                "Moore", "David",
+                "Taylor", "Emma",
+                "Anderson", "John",
+                "Thomas", "Olivia"
+        ));
+ */
