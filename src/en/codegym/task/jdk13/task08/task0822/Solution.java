9ac0b12699d7dbd8f3bc8773f3b1ae2921691165
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-09 10:53:04 +0200

    Task 232 (Level 14/Lesson 9/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task08/task0822/Solution.java
@@ -0,0 +1,28 @@
+package en.codegym.task.jdk13.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Minimum of N numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> list) {
+        // Find the minimum here
+        return 0;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        // Create and initialize a list here
+        return null;
+    }
+}
