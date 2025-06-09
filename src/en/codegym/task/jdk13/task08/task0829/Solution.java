558c83087d667ff10a54b08aae70fc15c7c7cad4
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-09 11:32:45 +0200

    Task 233 (Level 14/Lesson 9/Task 6) - My solution

--- src/en/codegym/task/jdk13/task08/task0829/Solution.java
+++ src/en/codegym/task/jdk13/task08/task0829/Solution.java
@@ -3,8 +3,6 @@ package en.codegym.task.jdk13.task08.task0829;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,22 +14,78 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<String> list = new ArrayList<>();
+        Map<String, String> cityAndFamily = new HashMap<>();
         while (true) {
-            String family = reader.readLine();
-            if (family.isEmpty()) {
+            //System.out.println("Enter the city name:");
+            String key = reader.readLine();
+            if (key.isEmpty()) {
+                break;
+            }
+            //System.out.println("Enter the last name of family living there:");
+            String value = reader.readLine();
+            if (value.isEmpty()) {
                 break;
             }
 
-            list.add(family);
+            cityAndFamily.put(key, value);
         }
 
-        // Read the house number
-        int houseNumber = Integer.parseInt(reader.readLine());
+        //System.out.println("What city are you interested in?");
+        String city = reader.readLine();
 
-        if (houseNumber >= 0 && houseNumber < list.size()) {
-            String familyName = list.get(houseNumber);
+        if (cityAndFamily.containsKey(city)) {
+            System.out.println(cityAndFamily.get(city));
+        }
+    }
+}
+
+/*
+Recommended solution
+
+Map<String, String> map = new HashMap<>();
+        while (true) {
+            String city = reader.readLine();
+            if (city.isEmpty()) {
+                break;
+            }
+            String family = reader.readLine();
+
+            map.put(city, family);
+        }
+
+        String city = reader.readLine();
+
+        if (map.containsKey(city)) {
+            String familyName = map.get(city);
             System.out.println(familyName);
         }
     }
 }
+
+My overly complex program does not pass validation
+
+ Map<String, String> cityAndFamily = new HashMap<>();
+        while (true) {
+            System.out.println("Enter the city name:");
+            String key = reader.readLine();
+            if (key.isEmpty()) {
+                break;
+            }
+            System.out.println("Enter the last name of family living there:");
+            String value = reader.readLine();
+            if (value.isEmpty()) {
+                break;
+            }
+
+            cityAndFamily.put(key, value);
+        }
+
+        System.out.println("What city are you interested in?");
+        String city = reader.readLine();
+
+        if (cityAndFamily.containsKey(city)) {
+            System.out.println(cityAndFamily.get(city));
+        }
+    }
+
+ */
