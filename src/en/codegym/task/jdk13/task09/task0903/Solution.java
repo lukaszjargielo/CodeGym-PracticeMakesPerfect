e25259a08fb709cda13b98bfd3c74695705681cb
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-11 11:07:53 +0200

    Task 236 (Level 15/Lesson 2/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0903/Solution.java
@@ -0,0 +1,36 @@
+package en.codegym.task.jdk13.task09.task0903;
+
+/* 
+Who called me?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        return  /*write your code here*/ 0;
+    }
+
+    public static int method2() {
+        method3();
+        return  /*write your code here*/ 0;
+    }
+
+    public static int method3() {
+        method4();
+        return  /*write your code here*/ 0;
+    }
+
+    public static int method4() {
+        method5();
+        return  /*write your code here*/ 0;
+    }
+
+    public static int method5() {
+        return  /*write your code here*/ 0;
+    }
+}
