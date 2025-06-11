a2a484e082683613b046ccd406072ae49eab0837
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-11 13:47:33 +0200

    Task 239 (Level 15/Lesson 2/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0905/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task09.task0905;
+
+/* 
+The method returns a result — the depth of its stack trace
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int deep = getStackTraceDepth();
+    }
+
+    public static int getStackTraceDepth() {
+        //write your code here
+    }
+}
+
