77e55b2cbd0e83b44a79281bbba56c1b95dad1af
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-11 13:46:00 +0200

    Task 238 (Level 15/Lesson 2/Task 5) - My solution

--- src/en/codegym/task/jdk13/task09/task0904/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0904/Solution.java
@@ -4,48 +4,72 @@ package en.codegym.task.jdk13.task09.task0904;
 Stack trace with 10 calls
 */
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
+
         int stackTraceLength = method1().length - method10().length + 1;
+
+        /*System.out.println(method1().length);
+        System.out.println(Arrays.toString(method1()));
+        System.out.println(method10().length);
+        System.out.println(Arrays.toString(method10()));*/
+
+
+        //System.out.println(stackTraceLength);
     }
 
     public static StackTraceElement[] method1() {
+
         return method2();
     }
 
     public static StackTraceElement[] method2() {
-        //write your code here
+
+        return method3();
     }
 
     public static StackTraceElement[] method3() {
-        //write your code here
+
+        return method4();
     }
 
     public static StackTraceElement[] method4() {
-        //write your code here
+
+        return method5();
     }
 
     public static StackTraceElement[] method5() {
-        //write your code here
+
+        return method6();
+
     }
 
     public static StackTraceElement[] method6() {
-        //write your code here
+
+        return method7();
+
     }
 
     public static StackTraceElement[] method7() {
-        //write your code here
+
+        return method8();
+
     }
 
     public static StackTraceElement[] method8() {
-        //write your code here
+
+        return method9();
     }
 
     public static StackTraceElement[] method9() {
+
         return method10();
     }
 
     public static StackTraceElement[] method10() {
+
         return Thread.currentThread().getStackTrace();
     }
 }
