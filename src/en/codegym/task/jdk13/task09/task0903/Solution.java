f11f668b9ee1925745d14d7b765105d2cc5a75d4
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-11 11:17:48 +0200

    Task 236 (Level 15/Lesson 2/Task 3) - My solution

--- src/en/codegym/task/jdk13/task09/task0903/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0903/Solution.java
@@ -11,26 +11,35 @@ public class Solution {
     }
 
     public static int method1() {
+
         method2();
-        return  /*write your code here*/ 0;
+
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
     }
 
     public static int method2() {
+
         method3();
-        return  /*write your code here*/ 0;
+
+        return  Thread.currentThread().getStackTrace()[2].getLineNumber();
     }
 
     public static int method3() {
+
         method4();
-        return  /*write your code here*/ 0;
+
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
     }
 
     public static int method4() {
+
         method5();
-        return  /*write your code here*/ 0;
+
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
     }
 
     public static int method5() {
-        return  /*write your code here*/ 0;
+
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
     }
 }
