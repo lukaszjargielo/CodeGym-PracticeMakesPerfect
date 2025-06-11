2f8d72dbc9f845bb212e158fd0749ff38448b4cc
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-11 10:59:21 +0200

    Task 235 (Level 15/Lesson 2/Task 2) - My solution

--- src/en/codegym/task/jdk13/task09/task0902/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0902/Solution.java
@@ -6,30 +6,83 @@ Stack trace revisited
 
 public class Solution {
     public static void main(String[] args) {
+
         method1();
+
+//        System.out.println("From method main");
+
     }
 
     public static String method1() {
+
         method2();
-        //write your code here
+
+        /*System.out.println("From method 1");
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (var el : stackTrace) {
+            System.out.println(el.getMethodName());
+        }
+        System.out.println();
+        return null;*/
+
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method2() {
+
         method3();
-        //write your code here
+
+        /*System.out.println("From method 2");
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (var el : stackTrace) {
+            System.out.println(el.getMethodName());
+        }
+        System.out.println();
+        return null;*/
+
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method3() {
+
         method4();
-        //write your code here
+
+        /*System.out.println("From method 3");
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (var el : stackTrace) {
+            System.out.println(el.getMethodName());
+        }
+        System.out.println();
+        return null;*/
+
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method4() {
+
         method5();
-        //write your code here
+
+        /*System.out.println("From method 4");
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (var el : stackTrace) {
+            System.out.println(el.getMethodName());
+        }
+        System.out.println();
+        return null;*/
+
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method5() {
-        //write your code here
+
+        /*StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        System.out.println("From method 5");
+        for (var el : stackTrace) {
+            System.out.println(el.getMethodName());
+        }
+        System.out.println();
+        return null;*/
+
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 }
