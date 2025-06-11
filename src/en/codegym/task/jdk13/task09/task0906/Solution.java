ccc51991872a1c257abc1ad3a00f0e16261607a4
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-11 11:24:30 +0200

    Task 237 (Level 15/Lesson 2/Task 4) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0906/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task09.task0906;
+
+/* 
+Logging stack traces
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        log("In method");
+    }
+
+    public static void log(String text) {
+        //write your code here
+    }
+}
