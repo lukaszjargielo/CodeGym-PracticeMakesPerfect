9eeaf12b1887eb921b5ffdc184ba14a54ebee904
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 10:07:53 +0200

    Task 243 (Level 15/Lesson 4/Task 4) - My solution

--- src/en/codegym/task/jdk13/task09/task0910/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0910/Solution.java
@@ -8,11 +8,11 @@ Exception when working with List collections
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-
-        ArrayList<String> list = new ArrayList<>();
-        String s = list.get(18);
-
-        //write your code here
+        try {
+            ArrayList<String> list = new ArrayList<>();
+            String s = list.get(18);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
     }
 }
