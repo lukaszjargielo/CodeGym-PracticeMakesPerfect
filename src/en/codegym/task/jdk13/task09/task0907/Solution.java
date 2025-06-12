0f83a724fccdd6fb9c216287177ee7d1fd3fa9ee
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 09:41:29 +0200

    Task 240 (Level 15/Lesson 4/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0907/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task09.task0907;
+
+/* 
+Exception when working with numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+        int a = 42 / 0;
+
+        //write your code here
+    }
+}
