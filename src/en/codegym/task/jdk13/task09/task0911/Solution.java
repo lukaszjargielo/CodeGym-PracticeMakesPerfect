ca432f83d66bd8f4e8b1dbf8e919ff866ff88ea1
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 10:11:51 +0200

    Task 244 (Level 15/Lesson 4/Task 5) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0911/Solution.java
@@ -0,0 +1,19 @@
+package en.codegym.task.jdk13.task09.task0911;
+
+import java.util.HashMap;
+
+/* 
+Exception when working with Map collections
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //write your code here
+
+        HashMap<String, String> map = new HashMap<>(null);
+        System.out.println(map);
+
+        //write your code here
+    }
+}
