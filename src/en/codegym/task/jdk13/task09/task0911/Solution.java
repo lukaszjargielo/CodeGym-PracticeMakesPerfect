019c218ac2d64cf11118fb5f646ad7e3dddec6f0
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 10:13:51 +0200

    Task 244 (Level 15/Lesson 4/Task 5) - My solution

--- src/en/codegym/task/jdk13/task09/task0911/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0911/Solution.java
@@ -9,11 +9,11 @@ Exception when working with Map collections
 public class Solution {
 
     public static void main(String[] args) {
-        //write your code here
-
-        HashMap<String, String> map = new HashMap<>(null);
-        System.out.println(map);
-
-        //write your code here
+       try {
+           HashMap<String, String> map = new HashMap<>(null);
+           System.out.println(map);
+       } catch (NullPointerException e) {
+           System.out.println(e);
+       }
     }
 }
