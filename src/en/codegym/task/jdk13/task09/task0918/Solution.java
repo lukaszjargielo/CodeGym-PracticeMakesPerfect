bb84accf26c9f494dade98df25cc6ad2277c35ad
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 12:41:19 +0200

    Task 251 (Level 15/Lesson 8/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0918/Solution.java
@@ -0,0 +1,23 @@
+package en.codegym.task.jdk13.task09.task0918;
+
+/* 
+Creating your own exceptions
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    static class MyException {
+    }
+
+    static class MyException2 {
+    }
+
+    static class MyException3 {
+    }
+
+    static class MyException4 {
+    }
+}
+
