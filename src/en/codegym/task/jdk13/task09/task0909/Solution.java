7a949b9e957366f7184c8b564d58b21aa174d883
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 10:03:39 +0200

    Task 242 (Level 15/Lesson 4/Task 3) - My solution

--- src/en/codegym/task/jdk13/task09/task0909/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0909/Solution.java
@@ -7,11 +7,12 @@ Exception when working with arrays
 public class Solution {
 
     public static void main(String[] args) {
-        //write your code here
+        try {
+            int[] array = new int[2];
+            array[8] = 5;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
 
-        int[] array = new int[2];
-        array[8] = 5;
-
-        //write your code here
     }
 }
