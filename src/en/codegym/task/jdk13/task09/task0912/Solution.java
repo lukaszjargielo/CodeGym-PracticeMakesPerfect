1b35b3cf58e2c1c3b17d4b0ef00f9933eca033f7
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 10:15:18 +0200

    Task 245 (Level 15/Lesson 4/Task 6) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0912/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task09.task0912;
+
+/* 
+Exception when working with numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+        int num = Integer.parseInt("XYZ");
+        System.out.println(num);
+
+        //write your code here
+    }
+}
