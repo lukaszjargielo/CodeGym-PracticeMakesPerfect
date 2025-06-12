9c1c4f7879c27b8e17f82e3cf5ba9c2390bf7d0a
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 09:51:17 +0200

    Task 241 (Level 15/Lesson 4/Task 2) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0908/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task09.task0908;
+
+/* 
+Exception while working with strings
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+        String s = null;
+        String m = s.toLowerCase();
+
+        //write your code here
+    }
+}
