d3628df1df43d8a329bf181cf068b586b8c7c8d2
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 19:55:57 +0200

    Task 251 (Level 15/Lesson 8/Task 6) - My solution

--- src/en/codegym/task/jdk13/task09/task0918/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0918/Solution.java
@@ -4,20 +4,37 @@ package en.codegym.task.jdk13.task09.task0918;
 Creating your own exceptions
 */
 
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) {
     }
 
-    static class MyException {
+    static class MyException extends IOException {
     }
 
-    static class MyException2 {
+    static class MyException2 extends RuntimeException{
     }
 
-    static class MyException3 {
+    static class MyException3 extends NumberFormatException {
     }
 
-    static class MyException4 {
+    static class MyException4 extends Exception{
     }
 }
 
+/*
+Recommended solution
+
+static class MyException extends Throwable {
+    }
+
+    static class MyException2 extends ClassNotFoundException {
+    }
+
+    static class MyException3 extends RuntimeException {
+    }
+
+    static class MyException4 extends IllegalStateException {
+    }
+ */
