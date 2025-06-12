1b34583a085257db92706790f6dbd50e1cade60e
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-12 12:28:00 +0200

    Task 250 (Level 15/Lesson 8/Task 5) - My solution

--- src/en/codegym/task/jdk13/task09/task0915/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0915/Solution.java
@@ -12,10 +12,24 @@ public class Solution {
     public static StatelessBean BEAN = new StatelessBean();
 
     public static void main(String[] args) {
-        handleExceptions();
+try {
+    handleExceptions();
+} catch (Exception e) {
+    BEAN.log(e);
+}
     }
 
-    public static void handleExceptions() {
+    public static void handleExceptions() throws FileSystemException {
+        try {
+            BEAN.throwExceptions();
+        } catch (FileSystemException e) {
+            BEAN.log(e);
+            throw e;
+        } catch (CharConversionException e) {
+            BEAN.log(e);
+        } catch (IOException e) {
+            BEAN.log(e);
+        }
     }
 
     public static class StatelessBean {
@@ -37,3 +51,25 @@ public class Solution {
         }
     }
 }
+
+/*
+Recommended solution
+public static void main(String[] args) {
+        try {
+            handleExceptions();
+        } catch (FileSystemException e) {
+            BEAN.log(e);
+        }
+    }
+
+    public static void handleExceptions() throws FileSystemException {
+        try {
+            BEAN.throwExceptions();
+        } catch (FileSystemException exception) {
+            BEAN.log(exception);
+            throw exception;
+        } catch (IOException exception) {
+            BEAN.log(exception);
+        }
+    }
+ */
