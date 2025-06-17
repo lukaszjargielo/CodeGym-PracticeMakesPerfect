2f3fbc0f922749477ff68f06b8f335322955c76b
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 13:33:28 +0200

    Task 261 (Level 16/Lesson 4/Task 7) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0926/Solution.java
@@ -0,0 +1,26 @@
+package en.codegym.task.jdk13.task09.task0926;
+
+import java.util.ArrayList;
+
+/* 
+List of number arrays
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<int[]> list = createList();
+        printList(list);
+    }
+
+    public static ArrayList<int[]> createList() {
+        //write your code here
+        return null;
+    }
+
+    public static void printList(ArrayList<int[]> list) {
+        for (int[] array : list) {
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
