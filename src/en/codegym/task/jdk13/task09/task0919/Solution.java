098d93e53af0f0cd792d74f23b6cc137d294c30f
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 12:25:18 +0200

    Task 255 (Level 16/Lesson 4/Task 1) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0919/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.jdk13.task09.task0919;
+
+/* 
+Dividing by zero
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divideByZero();
+    }
+}
