942a7ba8285e88c3e663de0dd3f7aa960fbad047
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 12:09:16 +0200

    Task 254 (Level 16/Lesson 3/Task 3) - Requirements

---
+++ src/en/codegym/task/pro/task15/task1503/Solution.java
@@ -0,0 +1,37 @@
+package en.codegym.task.pro.task15.task1503;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+There are a lot of resources
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = null;
+        BufferedReader bufferedReader = null;
+        try {
+            scanner = new Scanner(System.in);
+            String fileName = scanner.nextLine();
+            bufferedReader = Files.newBufferedReader(Path.of(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        }
+    }
+}
+
