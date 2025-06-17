1a8d346be3ea4fa3f1239a4bd4466ccda96efd78
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 12:37:18 +0200

    Task 257 (Level 16/Lesson 4/Task 3) - Requirements

---
+++ src/en/codegym/task/jdk13/task16/task1605/Solution.java
@@ -0,0 +1,27 @@
+package en.codegym.task.jdk13.task16.task1605;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/* 
+Throwing exceptions
+*/
+
+public class Solution {
+
+    public void method1() {
+
+    }
+
+    public void method2() {
+
+    }
+
+    public void method3() {
+
+    }
+
+    public void method4() {
+
+    }
+}
