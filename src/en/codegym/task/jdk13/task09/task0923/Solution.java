d49a89f50d3034312f7ce39db21fe841c576c6b1
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 13:18:38 +0200

    Task 258 (Level 16/Lesson 4/Task 4) - My solution

--- src/en/codegym/task/jdk13/task09/task0923/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0923/Solution.java
@@ -11,7 +11,23 @@ public class Solution {
     public static char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
 
     public static void main(String[] args) throws Exception {
-        //write your code here
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            char[] charsInString = reader.readLine().toCharArray();
+            String vowelsOutput = "";
+            String consonantsAndPunctuationMarksOutput = "";
+            for (char letter : charsInString) {
+                if (isVowel( letter)){
+                    vowelsOutput += letter + " ";
+                } else if (letter != ' '){
+                   consonantsAndPunctuationMarksOutput += letter + " ";
+                } else {
+                    continue;
+                }
+            }
+            System.out.println(vowelsOutput);
+            System.out.println(consonantsAndPunctuationMarksOutput);
+        }
+
     }
 
     // The method checks whether a letter is a vowel
@@ -25,3 +41,24 @@ public class Solution {
         return false;
     }
 }
+
+/*
+Recommended solution
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+
+        StringBuilder builder1 = new StringBuilder();
+        StringBuilder builder2 = new StringBuilder();
+        for (char character : string.toCharArray()) {
+            if (isVowel(character)) {
+                builder1.append(character).append(" ");
+            } else if (character != ' ') {
+                builder2.append(character).append(" ");
+            }
+        }
+
+        System.out.println(builder1);
+        System.out.println(builder2);
+    }
+ */
