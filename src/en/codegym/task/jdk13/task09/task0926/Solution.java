97fb0463d1defa49f37f884e6a9a388ff1171121
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 13:43:32 +0200

    Task 261 (Level 16/Lesson 4/Task 7) - My solution

--- src/en/codegym/task/jdk13/task09/task0926/Solution.java
+++ src/en/codegym/task/jdk13/task09/task0926/Solution.java
@@ -1,6 +1,7 @@
 package en.codegym.task.jdk13.task09.task0926;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 
 List of number arrays
@@ -14,8 +15,19 @@ public class Solution {
     }
 
     public static ArrayList<int[]> createList() {
-        //write your code here
-        return null;
+        ArrayList<int[]> numbers = new ArrayList<>(20);
+        int[] array1 = {0,1,2,3,4};
+        int[] array2 = {5,6};
+        int[] array3 = {7,8,9,10};
+        int[] array4 = {11,12,13,14,15,16,17};
+        int[] array5 = {};
+        numbers.add(array1);
+        numbers.add(array2);
+        numbers.add(array3);
+        numbers.add(array4);
+        numbers.add(array5);
+
+        return numbers;
     }
 
     public static void printList(ArrayList<int[]> list) {
@@ -24,3 +36,17 @@ public class Solution {
         }
     }
 }
+
+/*
+Recommended solution
+
+    public static ArrayList<int[]> createList() {
+        ArrayList<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2, 3, 4, 5});
+        list.add(new int[]{1, 2});
+        list.add(new int[]{1, 2, 3, 4});
+        list.add(new int[]{1, 2, 3, 4, 5, 6, 7});
+        list.add(new int[]{});
+        return list;
+    }
+ */
