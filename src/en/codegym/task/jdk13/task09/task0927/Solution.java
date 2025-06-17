5ad764f3daa0dbfa90f91e1f8e8ac63d3580405b
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-17 13:44:46 +0200

    Task 262 (Level 16/Lesson 4/Task 8) - Requirements

---
+++ src/en/codegym/task/jdk13/task09/task0927/Solution.java
@@ -0,0 +1,59 @@
+package en.codegym.task.jdk13.task09.task0927;
+
+import java.util.*;
+
+/* 
+Ten cats
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<String, Cat> map = createMap();
+        Set<Cat> set = convertMapToSet(map);
+        printCatSet(set);
+    }
+
+    public static Map<String, Cat> createMap() {
+        //write your code here
+    }
+
+    public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
+        //write your code here
+    }
+
+    public static void printCatSet(Set<Cat> set) {
+        for (Cat cat : set) {
+            System.out.println(cat);
+        }
+    }
+
+    public static class Cat {
+        public String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return "Cat " + this.name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Cat)) {
+                return false;
+            }
+            Cat cat = (Cat) o;
+            return Objects.equals(name, cat.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
