8660759aa7515f09cb0cc9ccd7f256da2afd99ca
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-19 14:51:47 +0200

    Task 10(273) (Level 1/Lesson 8/Task 5) - Requirements

---
+++ src/com/codegym/task/task11/task1110/Solution.java
@@ -0,0 +1,34 @@
+package com.codegym.task.task11.task1110;
+
+/* 
+Don't forget to encapsulate
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
