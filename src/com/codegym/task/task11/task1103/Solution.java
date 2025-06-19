e97e12d93d0e2f44673f828a48b8eac69878378a
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-19 14:10:50 +0200

    Task 3(266) (Level 1/Lesson 6/Task 3) - Requirements

---
+++ src/com/codegym/task/task11/task1103/Solution.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task11.task1103;
+
+/* 
+Interplanetary farm
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow {
+
+    }
+
+    public class Pig {
+
+    }
+
+    public class Sheep {
+
+    }
+
+    public class Bull {
+
+    }
+
+    public class Goat {
+
+    }
+
+}
