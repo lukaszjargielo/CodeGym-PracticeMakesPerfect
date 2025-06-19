b6079e56971ace4b0762cec979828d95b1b0049a
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-19 14:13:35 +0200

    Task 4(267) (Level 1/Lesson 6/Task 4) - Requirements

---
+++ src/com/codegym/task/task11/task1104/Solution.java
@@ -0,0 +1,27 @@
+package com.codegym.task.task11.task1104;
+
+/* 
+Hard workers
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager {
+
+    }
+
+    public class CEO {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary {
+
+    }
+}
