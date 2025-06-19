aa0d165d88daa854a79f95771910634a98108703
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-19 14:06:21 +0200

    Task 2(265) (Level 1/Lesson 6/Task 2) - Requirements

---
+++ src/com/codegym/task/task11/task1102/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task11.task1102;
+
+/* 
+Pets
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Dog {
+
+    }
+}
