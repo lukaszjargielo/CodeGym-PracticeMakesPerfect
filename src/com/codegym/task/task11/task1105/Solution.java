10085837096af8b9a4bc5f9c1b86b41eeb754e7d
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-19 14:19:05 +0200

    Task 5(268) (Level 1/Lesson 6/Task 5) - Requirements

---
+++ src/com/codegym/task/task11/task1105/Solution.java
@@ -0,0 +1,47 @@
+package com.codegym.task.task11.task1105;
+
+/* 
+IT company
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Employee {
+
+    }
+
+    public class Clerk {
+
+    }
+
+    public class ITSpecialist {
+
+    }
+
+    public class Programmer {
+
+    }
+
+    public class ProjectManager {
+
+    }
+
+    public class CTO {
+
+    }
+
+    public class OfficeManager {
+
+    }
+
+    public class Recruiter {
+
+    }
+
+    public class Custodian {
+
+    }
+}
