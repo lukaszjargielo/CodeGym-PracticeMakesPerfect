bf3eeb8b9018533e9e9f981fb7085227941af274
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-22 11:25:07 +0200

    Task 22(285) (Level 1/Lesson 11/Task 12) - My solution

--- src/com/codegym/task/task11/task1122/Solution.java
+++ src/com/codegym/task/task11/task1122/Solution.java
@@ -9,21 +9,25 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public class King {
+    public class King extends ChessPiece {
     }
 
-    public class Queen {
+    public class Queen extends ChessPiece{
     }
 
-    public class Rook {
+    public class Rook extends ChessPiece{
     }
 
-    public class Knight {
+    public class Knight extends ChessPiece{
     }
 
-    public class Bishop {
+    public class Bishop extends ChessPiece{
     }
 
-    public class Pawn {
+    public class Pawn extends ChessPiece{
+    }
+
+    public class ChessPiece {
+
     }
 }
