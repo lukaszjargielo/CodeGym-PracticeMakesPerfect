ee68c6293bd0f2f6f3d1ea5f662606481d4472f9
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-24 08:38:31 +0200

    Task 30(293) (Level 2/Lesson 4/Task 2) - My solution

--- src/com/codegym/task/task12/task1207/Solution.java
+++ src/com/codegym/task/task12/task1207/Solution.java
@@ -7,8 +7,23 @@ int and Integer
 
 public class Solution {
     public static void main(String[] args) {
+print(11);
+print(Integer.valueOf(11));
+    }
+
+    public static void print(int number) {
 
     }
 
-    //write your code here
+    public static void print(Integer number) {
+
+    }
 }
+/*
+Recommended solution
+
+public static void main(String[] args) {
+        print(new Integer(1));
+        print(2);
+    }
+ */
