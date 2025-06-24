8ee1635a3ed92f4b5eda4c329e6999064c5ebd34
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-24 08:45:36 +0200

    Task 31(294) (Level 2/Lesson 4/Task 3) - My solution

--- src/com/codegym/task/task12/task1208/Solution.java
+++ src/com/codegym/task/task12/task1208/Solution.java
@@ -5,10 +5,30 @@ Freedom of the press
 
 */
 
+import java.math.BigDecimal;
+
 public class Solution {
     public static void main(String[] args) {
 
     }
 
-    //write your code here
+    public static void print(String text) {
+        System.out.println(text);
+    }
+
+    public static void print(double number) {
+        System.out.println(number);
+    }
+
+    public static void print(int number) {
+        System.out.println(number);
+    }
+
+    public static void print(boolean value) {
+        System.out.println(value);
+    }
+
+     public static void print(BigDecimal value) {
+        System.out.println(value);
+    }
 }
