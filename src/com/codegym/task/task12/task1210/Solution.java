1152af575e814357a30c263c9c3b7e0398b533c4
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-24 08:59:35 +0200

    Task 33(296) (Level 2/Lesson 4/Task 5) - My solution

--- src/com/codegym/task/task12/task1210/Solution.java
+++ src/com/codegym/task/task12/task1210/Solution.java
@@ -10,5 +10,15 @@ public class Solution {
 
     }
 
-    //write your code here
+    public static int max(int number1, int number2) {
+        return number1 > number2 ? number1 : number2;
+    }
+
+    public static long max(long number1, long number2) {
+        return Math.max(number1, number2);
+    }
+
+    public static double max(double number1, double number2) {
+        return number1 > number2 ? number1 : number2;
+    }
 }
