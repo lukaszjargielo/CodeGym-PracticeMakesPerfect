419761a688968af2fc7d5b2367dc323037d0e365
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-28 11:53:46 +0200

    Task 37(300) (Level 2/Lesson 6/Task 4) - My solution

--- src/com/codegym/task/task12/task1214/Solution.java
+++ src/com/codegym/task/task12/task1214/Solution.java
@@ -14,7 +14,11 @@ public class Solution {
         public abstract String getName();
     }
 
-    public static class Cow {
+    public static class Cow extends Animal {
+        @Override
+        public String getName() {
+            return this.getClass().getSimpleName();
+        }
     }
 
 }
