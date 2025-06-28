de74ae5eac5fe81b5ab918e00134c4916fab47d2
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-28 13:20:47 +0200

    Task 45(308) (Level 2/Lesson 12/Task 2) - My solution

--- src/com/codegym/task/task12/task1222/Solution.java
+++ src/com/codegym/task/task12/task1222/Solution.java
@@ -30,6 +30,9 @@ public class Solution {
     }
 
     public static class Cat extends Pet {
-
+        @Override
+        public void setName(String name) {
+            this.name = "I'm a cat";
+        }
     }
 }
