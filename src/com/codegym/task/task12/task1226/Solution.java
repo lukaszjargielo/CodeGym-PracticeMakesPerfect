9d42e4526d0bb567ff5726cedc084d4bb62f3bc4
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-28 14:09:46 +0200

    Task 49(312) (Level 2/Lesson 12/Task 6) - My solution

--- src/com/codegym/task/task12/task1226/Solution.java
+++ src/com/codegym/task/task12/task1226/Solution.java
@@ -11,15 +11,51 @@ public class Solution {
 
     }
 
-    public class Cat {
+    public class Cat implements CanClimb, CanRun {
+        @Override
+        public void climb() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 
-    public class Dog {
+    public class Dog implements CanRun {
+
+        @Override
+        public void run() {
+
+        }
     }
 
     public class Tiger extends Cat {
     }
 
-    public class Duck {
+    public class Duck implements CanFly, CanRun {
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanClimb {
+        public void climb();
+    }
+
+    public interface CanRun {
+        public void run();
     }
 }
