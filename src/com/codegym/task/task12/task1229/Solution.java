fe8e4ec41dd5bd62c76a6ced7628f2cd890f1da1
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-30 10:31:56 +0200

    Task 52(315) (Level 2/Lesson 12/Task 9) - My solution

--- src/com/codegym/task/task12/task1229/Solution.java
+++ src/com/codegym/task/task12/task1229/Solution.java
@@ -16,7 +16,13 @@ public class Solution {
         public void workHard();
     }
 
-    public static class CTO implements Businessman {
+    public static class CTO extends Employee implements Businessman {
 
+    }
+
+    public static class Employee {
+        public void workHard() {
+
+        }
     }
 }
