2f4e624fbf801d432cb4ef943fca1343f9038108
lukaszjargielo <l.jargielo88@gmail.com>
2025-07-01 10:20:17 +0200

    Task 60(323) (Level 3/Lesson 2/Task 4) - Requirements

---
+++ src/com/codegym/task/task13/task1304/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task13.task1304;
+
+/* 
+Selectable and Updatable
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    //write your code here
+}
