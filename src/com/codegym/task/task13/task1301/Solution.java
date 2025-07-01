c8c2aed91ee0f1134a076e5023783049d682028c
lukaszjargielo <l.jargielo88@gmail.com>
2025-07-01 09:06:36 +0200

    Task 57(320) (Level 3/Lesson 2/Task 1) - Requirements

---
+++ src/com/codegym/task/task13/task1301/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task13.task1301;
+
+/* 
+Beer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Drinker {
+        void askForMore(String message);
+
+        void sayThankYou();
+
+        boolean isReadyToGoHome();
+    }
+
+    public interface Alcoholic extends Drinker {
+        boolean READY_TO_GO_HOME = false;
+
+        void sleepOnTheFloor();
+    }
+
+    public static class BeerLover {
+    }
+}
